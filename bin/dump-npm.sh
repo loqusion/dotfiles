d47756212fa6c75bd526de019df6776372f9079e
John Bernard <38332081+loqusion@users.noreply.github.com>
2022-05-25 09:43:59 -0500

    fix dump npm

--- bin/dump-npm.sh
+++ bin/dump-npm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 npm list -g --depth=0 --parseable |
-  sed 's/\/Users\/rasnauf\/.nvm\/versions\/node\/v18.1.0\/lib\/node_modules\///g' |
+  sed -E 's/\/Users\/rasnauf\/\.nvm\/versions\/node\/.*\/lib\/node_modules\///g' |
   sed 1d >~/.npm-packages
