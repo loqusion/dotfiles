f94f3b07b07880582244f6839130466338b30c20
John Bernard <38332081+loqusion@users.noreply.github.com>
2022-05-25 09:52:46 -0500

    fix dump-npm

--- bin/dump-npm.sh
+++ bin/dump-npm.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+escaped_home=$(echo "$HOME" | sed 's/\//\\\//g')
 npm list -g --depth=0 --parseable |
-  sed -E 's/\/Users\/rasnauf\/\.nvm\/versions\/node\/.*\/lib\/node_modules\///g' |
+  sed -E "s/${escaped_home}\/\.nvm\/versions\/node\/.*\/lib\/node_modules\///g" |
   sed 1d >~/.npm-packages
