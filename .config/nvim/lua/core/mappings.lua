15db53138ee4a458e6dee62953d641c069c83a15
John Bernard <38332081+loqusion@users.noreply.github.com>
2022-06-25 00:57:04 -0500

    [vim] move mappings to `core.mappings`

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -1,6 +1,79 @@
 local path = require 'utils.api.path'
 local key = require('crows').key
 
+local function c_ctrl_k()
+  vim.cmd [[exe 'normal! ' .. repeat("\<Del>", strchars(getcmdline()[getcmdpos() - 1:]))]]
+end
+
+local function ctrl_f()
+  vim.cmd [[exe 'normal! ' .. max([winheight(0) - 2, 1]) .. "\<C-e>" .. (line('w$') >= line('$') ? 'L' : 'M')]]
+end
+
+local function ctrl_b()
+  vim.cmd [[exe 'normal! ' .. max([winheight(0) - 2, 1]) .. "\<C-y>" .. (line('w0') <= 1 ? 'H' : 'M')]]
+end
+
+local function l()
+  vim.cmd [[exe 'normal! ' .. (foldclosed(line('.')) != -1 ? (mode() == 'n' ? 'zo0' : 'zogv0') : 'l')]]
+end
+
+-- Basic mappings
+key.maps({
+  ['>'] = { '>>', 'Indent current line' },
+  ['<'] = { '<<', 'Dedent current line' },
+  ['*'] = { '*N', 'Highlight matches' },
+  -- ['#'] = {  '#N' , 'Highlight matches',},
+  ['g*'] = { 'g*N', 'Highlight matches' },
+  ['g#'] = { 'g#N', 'Highlight matches' },
+  x = { '"_x', 'Delete character below cursor' },
+  X = { '"_X', 'Delete character before cursor' },
+  l = { l, 'which_key_ignore' },
+  q = { '<Nop>', '', mode = { 'n', 'x' } },
+  Q = { 'q', 'Start recording macro' },
+  M = { 'm', 'Set mark' },
+  ZZ = { '<Nop>', 'which_key_ignore' },
+  ['<C-c>'] = { '<silent> <C-c>', '' },
+  ['#'] = { '<C-^>', 'Switch to alternate buffer' },
+  gV = { 'v`[o`]', 'Select previously inserted/yanked text' },
+  ['<C-f>'] = { ctrl_f, 'Scroll down one page' },
+  ['<C-b>'] = { ctrl_b, 'Scroll up one page' },
+  ['<C-h>'] = { '<C-w>h', 'Move to left window' },
+  ['<C-j>'] = { '<C-w>j', 'Move to below window' },
+  ['<C-k>'] = { '<C-w>k', 'Move to above window' },
+  ['<C-l>'] = { '<C-w>l', 'Move to right window' },
+  ['<leader>'] = {
+    ['<leader>'] = { '<Cmd>silent update<CR>', 'Write (if modified)' },
+    q = { '<Cmd>qall<CR>', 'Exit Vim' },
+    x = { '<Cmd>xit!<CR>', 'Write (if modified) and close window' },
+  },
+}, {}, false)
+key.maps({
+  ['>'] = { '>gv', 'Indent selection' },
+  ['<'] = { '<gv', 'Dedent selection' },
+  p = { 'P', 'Put without yank to unnamed register' },
+}, { mode = 'x' }, false)
+key.maps({
+  ['<C-w>'] = { '<C-g>u<C-w>', '' },
+  ['<C-u>'] = { '<C-g>u<C-u>', '' },
+  -- ['<C-k>'] = { '<C-o>D', '' },
+  ['<C-CR>'] = { '<C-o>o', 'Begin new line below cursor' },
+  ['<S-CR>'] = { '<C-o>O', 'Begin new line above cursor' },
+}, { mode = 'i' }, false)
+key.maps({
+  ['<C-b>'] = { '<Left>', '' },
+  ['<C-f>'] = { '<Right>', '' },
+  ['<M-b>'] = { '<S-Left>', '' },
+  ['<M-f>'] = { '<S-Right>', '' },
+  ['<C-a>'] = { '<Home>', '' },
+  ['<C-e>'] = { '<End>', '' },
+  ['<C-p>'] = { '<Up>', '' },
+  ['<C-n>'] = { '<Down>', '' },
+  ['<C-y>'] = { '<C-r>*', '' },
+  ['<C-g>'] = { '<C-c>', '' },
+  ['<C-d>'] = { '<Del>', '' },
+  ['<C-k>'] = { c_ctrl_k, '' },
+}, { mode = 'c' }, false)
+
 ---@return string
 local function canonical_name(name)
   return string.match(name, '/([%w%-_]+).?')
@@ -14,6 +87,7 @@ local function get_config_path(name)
 end
 
 local function goto_plugin_config()
+  ---@diagnostic disable-next-line: missing-parameter
   local cfile = vim.fn.expand '<cfile>'
   local plugin_name = canonical_name(cfile) or cfile
   local config_path = get_config_path(plugin_name)
