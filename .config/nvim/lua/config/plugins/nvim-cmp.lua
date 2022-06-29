f8c72665d9d6a39c36c3854e28941dd79de73961
John Bernard <38332081+loqusion@users.noreply.github.com>
2022-06-29 16:03:30 -0500

    [vim] cmp config

--- .config/nvim/lua/config/plugins/nvim-cmp.lua
+++ .config/nvim/lua/config/plugins/nvim-cmp.lua
@@ -1,87 +1,89 @@
 -- https://github.com/hrsh7th/nvim-cmp
 
+local settings = require 'core.settings'
+
 local M = {}
 
 function M.setup() end
 
 function M.config()
-  local cmp = require 'cmp'
-  local luasnip = require 'luasnip'
-  local lspkind = require 'lspkind'
+  local ok, m = pcall(require, 'cmp')
+  if not ok then
+    vim.notify(
+      string.format('Failed to load config %s: %s', require('utils.api.path').current_filename(true), m),
+      vim.log.levels.ERROR
+    )
+    return
+  end
+
+  M.cmp = m
+
+  M.luasnip = require 'luasnip'
+  M.lspkind = require 'lspkind'
+
   local function tab(fallback)
-    if cmp.visible() then
-      cmp.select_next_item()
-    elseif luasnip.expand_or_jumpable() then
-      luasnip.expand_or_jump()
+    if M.luasnip.expand_or_jumpable() then
+      M.luasnip.expand_or_jump()
     else
       fallback()
     end
   end
 
   local function stab(fallback)
-    if cmp.visible() then
-      cmp.select_prev_item()
-    elseif luasnip.jumpable(-1) then
-      luasnip.jump(-1)
+    if M.luasnip.jumpable(-1) then
+      M.luasnip.jump(-1)
     else
       fallback()
     end
   end
 
-  cmp.setup {
+  M.cmp.setup {
     completion = {
-      completeopt = 'menuone,noselect',
-      -- completeopt = 'menu,menuone,noinsert',
+      completeopt = settings.opt.completeopt or 'menu,menuone,noselect',
     },
     sorting = {
       comparators = {
-        cmp.config.compare.offset,
-        cmp.config.compare.exact,
-        cmp.config.compare.score,
+        M.cmp.config.compare.offset,
+        M.cmp.config.compare.exact,
+        M.cmp.config.compare.score,
         require('cmp-under-comparator').under,
-        cmp.config.compare.kind,
-        cmp.config.compare.sort_text,
-        cmp.config.compare.length,
-        cmp.config.compare.order,
+        M.cmp.config.compare.kind,
+        M.cmp.config.compare.sort_text,
+        M.cmp.config.compare.length,
+        M.cmp.config.compare.order,
       },
     },
     snippet = {
       expand = function(args)
-        luasnip.lsp_expand(args.body)
+        M.luasnip.lsp_expand(args.body)
       end,
     },
     formatting = {
-      format = lspkind.cmp_format {
+      format = M.lspkind.cmp_format {
         mode = 'symbol',
         maxwidth = 50,
       },
     },
-    mapping = cmp.mapping.preset.insert {
-      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete {},
-      ['<C-e>'] = cmp.mapping {
-        i = cmp.mapping.abort(),
-        c = cmp.mapping.close(),
+    mapping = M.cmp.mapping.preset.insert {
+      ['<C-d>'] = M.cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = M.cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = M.cmp.mapping.complete(),
+      ['<C-e>'] = M.cmp.mapping {
+        i = M.cmp.mapping.abort(),
+        c = M.cmp.mapping.close(),
       },
-      ['<C-y>'] = cmp.mapping {
-        c = cmp.mapping.confirm { select = true },
+      ['<C-y>'] = M.cmp.mapping {
+        c = M.cmp.mapping.confirm { select = true },
       },
-      ['<CR>'] = cmp.mapping {
-        i = cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Replace,
-          select = true,
-        },
-        c = function(fallback)
-          fallback()
-        end,
-      },
-      ['<Tab>'] = cmp.mapping(tab, { 'i', 's' }),
-      ['<S-Tab>'] = cmp.mapping(stab, { 'i', 's' }),
+      ['<Tab>'] = M.cmp.mapping(tab, { 'i', 's' }),
+      ['<S-Tab>'] = M.cmp.mapping(stab, { 'i', 's' }),
     },
-    sources = cmp.config.sources({
+    experimental = {
+      ghost_text = true,
+    },
+    sources = M.cmp.config.sources({
       { name = 'npm', keyword_length = 4 },
-      --{ name = 'nvim_lsp_signature_help' },
+      { name = 'nvim_lsp_signature_help' },
       { name = 'nvim_lsp' },
       { name = 'luasnip' },
       { name = 'nvim_lua' },
@@ -91,26 +93,26 @@ function M.config()
     }),
   }
 
-  cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
+  M.cmp.setup.filetype('gitcommit', {
+    sources = M.cmp.config.sources({
       { name = 'cmp_git' },
     }, {
       { name = 'buffer' },
     }),
   })
 
-  cmp.setup.cmdline('/', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
+  M.cmp.setup.cmdline('/', {
+    mapping = M.cmp.mapping.preset.cmdline(),
+    sources = M.cmp.config.sources({
       { name = 'nvim_lsp_document_symbol' },
     }, {
       { name = 'buffer' },
     }),
   })
 
-  cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
+  M.cmp.setup.cmdline(':', {
+    mapping = M.cmp.mapping.preset.cmdline(),
+    sources = M.cmp.config.sources({
       { name = 'path' },
     }, {
       { name = 'cmdline' },
