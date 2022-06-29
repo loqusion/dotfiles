56859675024076ddb7dda97b3250729dcbcde64d
John Bernard <38332081+loqusion@users.noreply.github.com>
2022-06-29 15:49:50 -0500

    [vim] `path.current_filename`

--- .config/nvim/lua/utils/api/path.lua
+++ .config/nvim/lua/utils/api/path.lua
@@ -24,4 +24,20 @@ function path.is_exists(p)
   return vim.fn.filereadable(p) == 1
 end
 
+--[[
+Get current filename.
+
+Pass `true` to get the full path.
+--]]
+function path.current_filename(should_get_full_path)
+  should_get_full_path = should_get_full_path or false
+  local info = debug.getinfo(2, 'S')
+  local file = info.source
+  if should_get_full_path then
+    return file:match '^@*([^@]*)$'
+  else
+    return file:match '[^/]*.lua$'
+  end
+end
+
 return path
