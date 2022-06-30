96ad08ae90845057cd1b8c25f7d61cd854426703
John Bernard <38332081+loqusion@users.noreply.github.com>
2022-06-29 21:52:29 -0500

    [vim] update neorg config

--- .config/nvim/lua/config/plugins/neorg.lua
+++ .config/nvim/lua/config/plugins/neorg.lua
@@ -2,13 +2,6 @@
 
 local M = {}
 
-M.dirman_config = {
-  workspaces = {
-    work = '~/notes/work',
-    home = '~/notes/home',
-  },
-}
-
 function M.setup() end
 
 function M.config()
@@ -21,13 +14,43 @@ function M.config()
   M.neorg.setup {
     load = {
       ['core.defaults'] = {},
-      ['core.norg.dirman'] = { config = M.dirman_config },
-      ['core.norg.concealer'] = {},
+      ['core.norg.concealer'] = {
+        config = {
+          icon_preset = 'diamond',
+        },
+      },
+      ['core.norg.dirman'] = {
+        config = {
+          workspaces = {
+            main = '~/notes',
+            gtd = '~/notes/gtd',
+          },
+          default_workspace = 'main',
+        },
+      },
       ['core.norg.completion'] = {
         config = { engine = 'nvim-cmp' },
       },
+      ['core.gtd.base'] = {
+        config = {
+          workspace = 'gtd',
+        },
+      },
+      ['core.presenter'] = {
+        config = {
+          zen_mode = 'truezen',
+        },
+      },
+      ['core.norg.journal'] = {},
+      ['core.export'] = {},
+      ['core.export.markdown'] = {
+        config = {
+          extensions = 'all',
+        },
+      },
       ['core.integrations.nvim-cmp'] = {},
       ['core.integrations.telescope'] = {},
+      ['core.integrations.treesitter'] = {},
     },
   }
 end
