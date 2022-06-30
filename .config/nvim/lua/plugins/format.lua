10528f289eb3a8df7f01a2e0d34df37d739ae198
John Bernard <38332081+loqusion@users.noreply.github.com>
2022-06-30 17:42:44 -0500

    [vim] change format lazy load

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -3,7 +3,6 @@ local format = require('crows.utils').new_feat()
 format.use {
   'mhartington/formatter.nvim',
   cmd = { 'Format', 'FormatWrite' },
-  event = { 'BufWritePre' },
   setup = true,
   config = true,
 }
