be54163a86e02efed5d0071d35ec7aca95da3ade
John Bernard <38332081+loqusion@users.noreply.github.com>
2022-07-04 21:25:44 -0500

    [vim] only toggle yabai float in parent nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,13 @@
 pcall(require, 'impatient')
 
+-- Some features are disabled if I open neovim in a neovim terminal
+if vim.env.NVIM_IS_PARENT_OPEN then
+  vim.env.NVIM_IS_CHILD_PROCESS = true
+else
+  vim.env.NVIM_IS_PARENT_OPEN = true
+  vim.env.NVIM_IS_CHILD_PROCESS = false
+end
+
 require 'core.options'
 require 'core.settings'
 require 'core.mappings'
--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -1,28 +1,39 @@
+local function yabai_float(action)
+  if vim.env.NVIM_IS_CHILD_PROCESS ~= 'true' then
+    vim.cmd([[silent !yabai-float ]] .. action)
+  end
+end
+local function yabai_float_on()
+  yabai_float 'on'
+end
+local function yabai_float_off()
+  yabai_float 'off'
+end
+
 local yabai = vim.api.nvim_create_augroup('yabai', {})
--- TODO: handle nested vims
 vim.api.nvim_create_autocmd('VimEnter', {
   group = yabai,
   pattern = '*',
-  command = 'silent !yabai-float off',
+  callback = yabai_float_off,
   desc = 'Make terminal full-screen',
   once = true,
 })
 vim.api.nvim_create_autocmd('VimResume', {
   group = yabai,
   pattern = '*',
-  command = 'silent !yabai-float off',
+  callback = yabai_float_off,
   desc = 'Make terminal full-screen',
 })
 vim.api.nvim_create_autocmd('VimSuspend', {
   group = yabai,
   pattern = '*',
-  command = 'silent !yabai-float on',
+  callback = yabai_float_on,
   desc = 'Make terminal float',
 })
 vim.api.nvim_create_autocmd('VimLeave', {
   group = yabai,
   pattern = '*',
-  command = 'silent !yabai-float on',
+  callback = yabai_float_on,
   desc = 'Make terminal float',
   once = true,
 })
