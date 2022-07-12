501dc83364da9b30bad5528655434d84178d23e6
John Bernard <38332081+loqusion@users.noreply.github.com>
2022-07-12 08:10:45 -0500

    [vim] lsp signature config

--- .config/nvim/lua/config/plugins/lsp_signature.lua
+++ .config/nvim/lua/config/plugins/lsp_signature.lua
@@ -5,8 +5,13 @@ local M = {}
 function M.setup() end
 
 function M.config()
-  require('crows.lsp').add_on_attach(function(_, _)
-    require('lsp_signature').on_attach { bind = true, handler_opts = { border = 'none' } }
+  require('crows.lsp').add_on_attach(function(_, bufnr)
+    require('lsp_signature').on_attach({
+      bind = true,
+      handler_opts = {
+        border = 'none',
+      },
+    }, bufnr)
   end)
 end
 
