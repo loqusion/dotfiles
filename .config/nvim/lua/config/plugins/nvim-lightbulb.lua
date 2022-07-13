aac871ca6edc9a043650e6d342d4b734c39d0851
John Bernard <38332081+loqusion@users.noreply.github.com>
2022-07-13 15:22:41 -0500

    [vim] change lightbulb to signcolumn

--- .config/nvim/lua/config/plugins/nvim-lightbulb.lua
+++ .config/nvim/lua/config/plugins/nvim-lightbulb.lua
@@ -5,15 +5,31 @@ local M = {}
 function M.setup()
   require('crows.lsp').add_on_attach(function(_, bufnr)
     local group = vim.api.nvim_create_augroup('lsp_aucmds', {})
-    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI', 'CursorMoved', 'CursorMovedI' }, {
       group = group,
       buffer = bufnr,
       callback = function()
         local action_icon = require('utils.icons').lsp_hover.Action
         require('nvim-lightbulb').update_lightbulb {
-          sign = { enabled = false },
-          virtual_text = { enabled = true, text = action_icon },
-          float = { enabled = false, text = action_icon, win_opts = { winblend = 100, anchor = 'NE' } },
+          sign = {
+            enabled = true,
+            text = action_icon,
+            priority = 15,
+          },
+          float = {
+            enabled = false,
+            text = action_icon,
+            win_opts = {},
+          },
+          virtual_text = {
+            enabled = false,
+            text = action_icon,
+          },
+          status_text = {
+            enabled = false,
+            text = action_icon,
+            text_unavailable = '',
+          },
         }
       end,
       desc = 'Show lightbulb',
