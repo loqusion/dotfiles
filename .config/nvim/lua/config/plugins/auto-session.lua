d6be46136dd459dc3765b9798ba6f964968f8cf9
John Bernard <38332081+loqusion@users.noreply.github.com>
2022-07-18 10:41:13 -0500

    [vim] close Trouble when leaving session

--- .config/nvim/lua/config/plugins/auto-session.lua
+++ .config/nvim/lua/config/plugins/auto-session.lua
@@ -10,7 +10,7 @@ function M.setup() end
 
 function M.config()
   M.auto_session.setup {
-    pre_save_cmds = { 'Neotree close' },
+    pre_save_cmds = { 'Neotree close', 'TroubleClose' },
     auto_session_suppress_dirs = { '~', '~/repos', '~/notes' },
   }
   M.register_global_keys()
