21d4ace71abfa4ba2f82a7988636bdbb25c10650
John Bernard <38332081+loqusion@users.noreply.github.com>
2022-07-28 19:30:37 -0500

    [script] edit dump-npm

--- bin/dump-npm.sh
+++ bin/dump-npm.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env sh
 
 cmd='npm'
-nvm_path="${HOME}/.nvm"
+nvm_dir="${NVM_DIR:-${HOME}/.config/nvm}"
 
-if ! type nvm >/dev/null 2>&1 && [ -r "${nvm_path}/nvm.sh" ]; then
-  . "${nvm_path}/nvm.sh"
+if ! type nvm >/dev/null 2>&1 && [ -r "${nvm_dir}/nvm.sh" ]; then
+  . "${nvm_dir}/nvm.sh"
 fi
 
 if type nvm >/dev/null 2>&1 && [ "$(nvm version current)" != "$(nvm version node)" ]; then
   cmd='nvm exec node npm'
 fi
 
-escaped_nvm_dir=$(echo "$NVM_DIR" | sed 's/\//\\\//g')
 {
   ${cmd} list -g --depth=0 --parseable |
-  sed -E '1d' |
-  sed -E "s/${escaped_nvm_dir}\/versions\/node\/.*\/lib\/node_modules\///" |
-  sed -E '/^corepack$/d'
+  sed -E -e '1d' -e "s,${nvm_dir}/versions/node/.*/lib/node_modules/,," -e '/^corepack$/d'
 } >~/.npm-packages
