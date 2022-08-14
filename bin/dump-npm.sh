23b403cc97c8cfe4d01859c86b6175da6af0aa1c
John Bernard <38332081+loqusion@users.noreply.github.com>
2022-08-14 14:45:03 -0500

    [scripts] update scripts

--- bin/dump-npm.sh
+++ bin/dump-npm.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-cmd='npm'
+cmd=(npm)
 nvm_dir="${NVM_DIR:-${HOME}/.config/nvm}"
 
 if ! type nvm >/dev/null 2>&1 && [ -r "${nvm_dir}/nvm.sh" ]; then
@@ -8,10 +8,10 @@ if ! type nvm >/dev/null 2>&1 && [ -r "${nvm_dir}/nvm.sh" ]; then
 fi
 
 if type nvm >/dev/null 2>&1 && [ "$(nvm version current)" != "$(nvm version node)" ]; then
-  cmd='nvm exec node npm'
+  cmd=(nvm exec node npm)
 fi
 
 {
-  ${cmd} list -g --depth=0 --parseable |
+  "${cmd[@]}" list -g --depth=0 --parseable |
   sed -E -e '1d' -e "s,${nvm_dir}/versions/node/.*/lib/node_modules/,," -e '/^corepack$/d'
 } >~/.npm-packages
