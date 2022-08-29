5237bea6eed150e418d5071aa65f4f9d30e65cf6
loqusion <loqusion@gmail.com>
2022-08-29 12:52:32 -0500

    Create install-deps.sh

---
+++ install-deps.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+exists ()
+{
+  command -v "$@" &>/dev/null
+}
+
+install_brew_deps ()
+{
+  brew bundle --global
+}
+
+install_npm_deps ()
+{
+  if exists npm &>/dev/null && [ -r ~/.npm-packages ]; then
+    npm install --global $(cat ~/.npm-packages)
+  fi
+}
+
+install_pip_deps ()
+{
+  if exists pip3 && [ -r ~/.requirements.txt ]; then
+    pip3 install --requirement ~/.requirements.txt
+  fi
+}
+
+install_rust ()
+{
+  if ! exists cargo; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  fi && {
+    # todo
+    true
+  }
+}
