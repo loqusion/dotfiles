59aa0096ce94c67f5c23b9fc725f446b3062cb5e
loqusion <loqusion@gmail.com>
2022-08-29 12:53:39 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ alias config='/usr/bin/git --git-dir="$DEST" --work-tree=$HOME'
 git clone --bare git@github.com:loqusion/dotfiles.git "$DEST"
 
 config config --local core.sparseCheckout true
-printf '/*\n!README.md\n!install.sh\n' > "$DEST/info/sparse-checkout"
+printf '/*\n!README.md\n!install.sh\n!install-deps.sh\n' > "$DEST/info/sparse-checkout"
 config checkout
 config config --local status.showUntrackedFiles no
 config config --local branch.main.remote origin
@@ -22,8 +22,3 @@ config config --local branch.main.merge refs/heads/main
 if ! which brew >/dev/null 2>&1; then
   bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
-
-# Install from ~/.Brewfile
-brew bundle --global
-
-# TODO: Also install with npm, pip, etc
