74c283caf3d4ec20eaa544816ab1d44578fc6a88
John Bernard <38332081+loqusion@users.noreply.github.com>
2022-09-02 14:15:50 -0500

    [vim] fix devicons marked as opt

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -2,7 +2,7 @@ local statusline = require('crows.utils').new_feat()
 
 statusline.use {
   'nvim-lualine/lualine.nvim',
-  requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+  requires = { 'kyazdani42/nvim-web-devicons' },
   config = true,
 }
 
