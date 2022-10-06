7cf7356dd684cf02d762c3af42d1cf76c8079a4e
John Bernard <38332081+loqusion@users.noreply.github.com>
2022-10-06 10:47:25 -0500

    [vim] better theme config

--- .config/nvim/lua/config/plugins/catppuccin.lua
+++ .config/nvim/lua/config/plugins/catppuccin.lua
@@ -1,6 +1,6 @@
 -- https://github.com/catppuccin/nvim
 
-local options = require('core.options')
+local options = require 'core.options'
 
 local flavors = {
   frappe = 'frappe',
@@ -12,7 +12,7 @@ local M = {
   safe_requires = {
     'catppuccin',
   },
-  flavor = flavors.macchiato,
+  flavor = flavors.mocha,
 }
 
 function M.setup() end
@@ -22,7 +22,16 @@ function M.config()
   M.catppuccin.setup {
     transparent_background = options.transparent_background,
   }
+
+  M.register_global_autocmds()
   vim.cmd [[colorscheme catppuccin]]
+end
+
+function M.register_global_autocmds()
+  vim.api.nvim_create_autocmd('ColorScheme', {
+    pattern = 'catppuccin',
+    command = [[hi CursorLine guibg=#303347 ]],
+  })
 end
 
 return M
