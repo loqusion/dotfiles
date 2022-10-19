89ab48166c5a48ada5c6fd45325ad8aec1880837
John Bernard <38332081+loqusion@users.noreply.github.com>
2022-10-19 11:28:49 -0500

    [script] remove debug guard

--- bin/dump-pip.sh
+++ bin/dump-pip.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if true || ! hash pipx &>/dev/null; then
+if ! hash pipx &>/dev/null; then
   echo "pipx does not exist. Make sure it's installed with \"brew install pipx\"."
   exit 1
 fi
