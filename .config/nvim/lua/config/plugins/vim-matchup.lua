dfb0671508bca1b7e1fafda96954018b6f69ec39
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-02-11 13:56:17 -0600

    [vim] no more matchup statusline

--- .config/nvim/lua/config/plugins/vim-matchup.lua
+++ .config/nvim/lua/config/plugins/vim-matchup.lua
@@ -7,6 +7,7 @@ function M.entrance()
   vim.g.matchup_matchparen_deferred_show_delay = 100
   vim.g.matchup_matchparen_hi_surround_always = 1
   vim.g.matchup_surround_enabled = 1
+  vim.g.matchup_matchparen_offscreen = {}
 end
 
 return M
