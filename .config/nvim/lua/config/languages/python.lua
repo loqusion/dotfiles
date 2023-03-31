d808c47c348343dcf13f7c2ffb2287ae72d8c129
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-03-31 05:23:46 -0500

    [vim] add virtualenv detection for pyright lsp

--- .config/nvim/lua/config/languages/python.lua
+++ .config/nvim/lua/config/languages/python.lua
@@ -1,7 +1,41 @@
+local util = require 'lspconfig/util'
+
+local path = util.path
+
 local python = {}
 
+local function get_python_path(workspace)
+  -- Use activated virtualenv.
+  if vim.env.VIRTUAL_ENV then
+    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+  end
+
+  do -- Find and use virtualenv via poetry in workspace directory.
+    local match = vim.fn.glob(path.join(workspace, 'poetry.lock'))
+    if match ~= '' then
+      local venv = vim.fn.trim(vim.fn.system 'poetry env info -p')
+      return path.join(venv, 'bin', 'python')
+    end
+  end
+
+  -- Find and use virtualenv in workspace directory.
+  for _, pattern in ipairs { '*', '.*' } do
+    local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
+    if match ~= '' then
+      return path.join(path.dirname(match), 'bin', 'python')
+    end
+  end
+
+  -- Fallback to system Python.
+  return vim.fn.exepath 'python3' or vim.fn.exepath 'python' or 'python'
+end
+
 python.lsp_configs = {
-  pyright = {},
+  pyright = {
+    before_init = function(_, config)
+      config.settings.python.pythonPath = get_python_path(config.root_dir)
+    end,
+  },
 }
 
 return python
