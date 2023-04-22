d5b5617ac013688c7480f41d71beb9758d34740b
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-04-22 06:53:23 -0500

    [vim] init config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,21 @@
+---@type any
+local notify = false
 -- vim.loader = false
+
+if notify then
+  notify = function(...)
+    local args = { ... }
+    vim.schedule(function()
+      vim.notify(unpack(args))
+    end)
+  end
+else
+  notify = function() end
+end
+
 if vim.loader then
   vim.loader.enable()
-  vim.schedule(function()
-    vim.notify("nvim loader is enabled")
-  end)
+  notify("nvim loader is enabled")
 end
 
 local debug = require("utils.debug")
@@ -11,6 +23,17 @@ local debug = require("utils.debug")
 if vim.env.VIMCONFIG then
   return debug.switch(vim.env.VIMCONFIG)
 end
+
+-- require("utils.profile").start()
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyVimStarted",
+  callback = function()
+    vim.schedule(function()
+      -- require("lazy.core.cache")._inspect()
+    end)
+  end,
+})
 
 require("config.lazy")
 
