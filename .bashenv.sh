cb44e47fe305c52646c10884054f616e8ae101a2
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-04-25 21:02:56 -0500

    [shell] remove obsolete shell config

--- .bashenv.sh
+++
@@ -1,4 +0,0 @@
-config="${XDG_CONFIG_HOME:-$HOME/.config}/shell"
-
-shopt -s expand_aliases
-source "${config}/aliases.sh"
--- .config/shell/rc-utils.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-_rc_exists () {
-    hash "$@" &>/dev/null
-}
