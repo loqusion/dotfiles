fea78ffb1b1777d1105b9f50c0599273cdd817fb
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-05-04 21:23:22 -0500

    fish, btop, stylua

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,24 @@
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t -- $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -f backward-delete-char history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
 function fish_user_key_bindings
-  fzf_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+    fzf_key_bindings
 end
