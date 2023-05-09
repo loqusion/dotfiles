5e257b19fe479874758d6067d1fd3b8b01e5a986
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-05-09 18:55:49 -0500

    [vim] refactor

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,5 +1,20 @@
 local Utils = require("utils")
 
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = Utils.augroup("close_with_q", "lazyvim"),
+  pattern = {
+    "OverseerList",
+    "fugitive",
+    "fugitiveblame",
+    "neotest-*",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
 -- show cursor line only in active window
 local excluded_filetypes = { ["neo-tree"] = true }
 vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
@@ -42,20 +57,5 @@ vim.api.nvim_create_autocmd("BufWritePost", {
     vim.notify("Reloading yabai+skhd")
     vim.fn.system('launchctl kickstart -k "gui/${UID}/homebrew.mxcl.yabai"')
     vim.fn.system('launchctl kickstart -k "gui/${UID}/homebrew.mxcl.skhd"')
-  end,
-})
-
--- close some filetypes with <q>
-vim.api.nvim_create_autocmd("FileType", {
-  group = Utils.augroup("close_with_q", "lazyvim"),
-  pattern = {
-    "OverseerList",
-    "fugitive",
-    "fugitiveblame",
-    "neotest-*",
-  },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
   end,
 })
