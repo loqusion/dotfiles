7a7d6f63c43d8a7ab789210896b5a57662b7fc8c
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-05-09 18:57:21 -0500

    [vim] edit neotest virtual_text

--- .config/nvim/lua/plugins/tasks.lua
+++ .config/nvim/lua/plugins/tasks.lua
@@ -51,6 +51,20 @@ return {
         quickfix = { open = false },
       }
     end,
+    config = function(_, opts)
+      -- get neotest namespace (api call creates or returns namespace)
+      local neotest_ns = vim.api.nvim_create_namespace("neotest")
+      vim.diagnostic.config({
+        virtual_text = {
+          format = function(diagnostic)
+            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+            return message
+          end,
+        },
+      }, neotest_ns)
+
+      require("neotest").setup(opts)
+    end,
   },
   "haydenmeade/neotest-jest",
   "nvim-neotest/neotest-python",
