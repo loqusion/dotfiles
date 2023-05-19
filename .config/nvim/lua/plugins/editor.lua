f013096439fb113399ae37de7e7c6465a70a01b2
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-05-19 09:45:14 -0500

    feat(vim): package-info

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -236,4 +236,17 @@ return {
     event = "VeryLazy",
     opts = {},
   },
+
+  {
+    "vuki656/package-info.nvim",
+    event = "BufRead package.json",
+    opts = {
+      hide_up_to_date = true,
+      package_manager = "pnpm",
+    },
+    config = function(_, opts)
+      require("package-info").setup(opts)
+      require("telescope").load_extension("package_info")
+    end,
+  },
 }
