8a18df20b443a75e2d53d87cb6a4ee4a634f2869
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-05-19 03:52:28 -0500

    feat(vim): clangd lsp

--- .config/nvim/lua/plugins/extras/lang/cpp.lua
+++ .config/nvim/lua/plugins/extras/lang/cpp.lua
@@ -2,6 +2,10 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      servers = {
+        clangd = {},
+        cmake = {},
+      },
       setup = {
         clangd = function(_, opts)
           opts.capabilities.offsetEncoding = { "utf-16" }
