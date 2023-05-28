2daa4eb833816fc830d22f405fdf31ff13789e69
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-05-27 20:26:27 -0500

    fix(vim): statuscolumn spacing

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -327,7 +327,7 @@ return {
             condition = { true, builtin.not_empty },
             click = "v:lua.ScLa",
           },
-          { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+          { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
           { text = { "%s" }, click = "v:lua.ScSa" },
         },
       })
