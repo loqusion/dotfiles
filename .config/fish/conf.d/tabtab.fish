c84c3788704131bd85fd9b4a7316956e9219f5eb
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-05-29 19:25:33 -0500

    refactor(shell): move tabtab config

---
+++ .config/fish/conf.d/tabtab.fish
@@ -0,0 +1,3 @@
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -35,7 +35,3 @@ abbr caf "caffeinate -d"
 abbr tarify "tar czpvf"
 abbr untar "tar xvf"
 abbr glow "glow -p"
-
-# tabtab source for packages
-# uninstall by removing these lines
-[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
