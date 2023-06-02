eec416e4384cb3b6af65d27e5f95959648d7ea69
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-06-02 04:48:00 -0500

    feat(shell): homebrew-command-not-found

--- .config/fish/conf.d/macos.fish
+++ .config/fish/conf.d/macos.fish
@@ -8,4 +8,9 @@ if test (uname) = Darwin
     set -gx CC (which clang)
     set -gx LDFLAGS -L/opt/homebrew/opt/llvm/lib
     set -gx CPPFLAGS -I/opt/homebrew/opt/llvm/include
+
+    set HB_CNF_HANDLER (brew --repository)"/Library/Taps/homebrew/homebrew-command-not-found/handler.fish"
+    if test -f $HB_CNF_HANDLER
+        source $HB_CNF_HANDLER
+    end
 end
