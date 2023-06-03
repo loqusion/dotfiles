25a62a35e6f4a2f10e700fbb56eb6e2feb0e2adc
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-06-03 06:21:06 -0500

    fix(vim): undo lazyvim terminal mappings

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,15 +11,24 @@ local function map(mode, lhs, rhs, opts)
   end
 end
 
-local function l()
-  vim.cmd([[exe 'normal! ' .. (foldclosed(line('.')) != -1 ? (mode() == 'n' ? 'zo0' : 'zogv0') : v:count1 .. 'l')]])
+local function unmap(modes, lhs, opts)
+  vim.keymap.del(modes, lhs, opts)
 end
+
+unmap("t", "<C-h>")
+unmap("t", "<C-j>")
+unmap("t", "<C-k>")
+unmap("t", "<C-l>")
+unmap("t", "<C-w>")
 
 map({ "n", "i", "c", "v", "o", "t", "l" }, "<MiddleMouse>", "<Nop>", { desc = "which_key_ignore" })
 
 map({ "n", "x" }, "x", '"_x', { desc = "Delete char" })
 map({ "n", "x" }, "X", '"_X', { desc = "Delete prev char" })
 
+local function l()
+  vim.cmd([[exe 'normal! ' .. (foldclosed(line('.')) != -1 ? (mode() == 'n' ? 'zo0' : 'zogv0') : v:count1 .. 'l')]])
+end
 map("n", "l", l, { desc = "which_key_ignore" })
 
 map("n", "gV", "v`[o`]", { desc = "Switch to VISUAL using last insertion/yank" })
