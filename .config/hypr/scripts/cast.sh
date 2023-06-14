cdafbc7b26386b68b156e29c0185a85b9a04bada
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-06-13 22:55:06 -0500

    feat(hyprland): better cast

--- .config/hypr/scripts/cast.sh
+++ .config/hypr/scripts/cast.sh
@@ -3,11 +3,19 @@
 SLURP_OPTS=${SLURP_OPTS:-"-d -F 'Terminess TTF Nerd Font'"}
 slurp="slurp $SLURP_OPTS"
 
+notify() {
+	notify-send -t 3000 -a 'wf-recorder' -e 'wf-recorder' "$@"
+}
+
 if pkill -INT wf-recorder; then
-	notify-send -t 3000 -a 'wf-recorder' -e 'wf-recorder' 'Video Captured'
+	notify 'Video Captured'
 else
 	XDG_VIDEOS_DIR="$(xdg-user-dir VIDEOS)"
 	date=$(date +%Y-%m-%d_%H-%M-%S)
 	geometry="$($slurp)"
+	if ! $?; then
+		notify 'Cancelled'
+		exit 1
+	fi
 	wf-recorder -c libx264rgb -g "$geometry" -f "$XDG_VIDEOS_DIR/$date.mp4"
 fi
