0d6e1c209226dc4d95d0bed5db84723ce34440da
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-06-21 21:15:45 -0500

    feat(vim): add jsdoc to required treesitter parsers for typescript

--- .config/nvim/lua/plugins/extras/lang/typescript.lua
+++ .config/nvim/lua/plugins/extras/lang/typescript.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "jsdoc" })
+      end
+    end,
+  },
+
+  {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
       local inlay_hints = {
