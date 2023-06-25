b04c68015fc5223784833d087e0a398a4a7ed6f0
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-06-25 03:34:03 -0500

    feat(vim): add autocmd for reloading launch.json

--- .config/nvim/lua/plugins/extras/dap/extensions.lua
+++ .config/nvim/lua/plugins/extras/dap/extensions.lua
@@ -1,9 +1,17 @@
+local function load_launchjs()
+  require("dap.ext.vscode").load_launchjs(nil, { rt_lldb = { "rust" } })
+end
+
 return {
   {
     "mfussenegger/nvim-dap",
     -- HACK: using `opts` for side effects without overriding `config`
     opts = function()
-      require("dap.ext.vscode").load_launchjs(nil, { rt_lldb = { "rust" } })
+      load_launchjs()
+      vim.api.nvim_create_autocmd("BufWritePost", {
+        pattern = "launch.json",
+        callback = load_launchjs,
+      })
     end,
   },
 }
