929b9fc604945a2fcf521909cda66d892f95b60d
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-06-30 10:17:50 -0500

    feat(vim): use lazyvim rust

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
     { import = "lazyvim.plugins.extras.test.core" },
--- .config/nvim/lua/plugins/extras/lang/rust.lua
+++ .config/nvim/lua/plugins/extras/lang/rust.lua
@@ -1,22 +1,5 @@
 local Utils = require("utils")
 
-local function get_codelldb_paths()
-  local this_os = vim.loop.os_uname().sysname
-  local extension_path = vim.env.MASON .. "/packages/codelldb/extension"
-  local codelldb_path
-  local liblldb_path
-
-  if this_os:find("Windows") then
-    codelldb_path = extension_path .. "/adapter/codelldb.exe"
-    liblldb_path = extension_path .. "/lldb/bin/liblldb.dll"
-  else
-    codelldb_path = extension_path .. "/adapter/codelldb"
-    liblldb_path = extension_path .. "/lldb/lib/liblldb" .. (this_os == "Linux" and ".so" or ".dylib")
-  end
-
-  return codelldb_path, liblldb_path
-end
-
 local function dap_rust_program()
   local target_dir = vim.fn.json_decode(vim.fn.system("cargo metadata --format-version 1 | jq '.target_directory'"))
   local target_debug_dir = ("%s/debug"):format(target_dir)
@@ -31,7 +14,10 @@ local function dap_rust_program()
     return vim.loop.fs_access(full_path, "X") or false
   end, { path = target_debug_dir, type = "file", limit = math.huge })
   if #executable_names == 0 then
-    vim.notify(("Could not find any executables in %s"):format(target_debug_dir), vim.log.levels.WARN)
+    vim.notify(
+      ("Could not find any executables in %s.\nTry running `cargo build`"):format(target_debug_dir),
+      vim.log.levels.WARN
+    )
     return
   end
 
@@ -40,49 +26,27 @@ end
 
 return {
   {
+    "Saecki/crates.nvim",
+    opts = {
+      null_ls = { enabled = true },
+    },
+  },
+
+  {
     "williamboman/mason.nvim",
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "rust-analyzer",
-        "rustfmt",
-      })
+      vim.list_extend(opts.ensure_installed, { "rustfmt" })
     end,
   },
 
   {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      { "simrat39/rust-tools.nvim", config = false },
-    },
+    "simrat39/rust-tools.nvim",
+    lazy = true,
     opts = {
-      servers = {
-        rust_analyzer = {
-          settings = {
-            ["rust-analyzer"] = {
-              procMacro = { enable = true },
-              cargo = { allFeatures = true },
-              checkOnSave = {
-                command = "clippy",
-                extraArgs = { "--no-deps" },
-              },
-            },
-          },
+      tools = {
+        inlay_hints = {
+          auto = false,
         },
-      },
-      setup = {
-        rust_analyzer = function(_, opts)
-          local codelldb_path, liblldb_path = get_codelldb_paths()
-          require("rust-tools").setup({
-            tools = {
-              inlay_hints = { auto = not Utils.has("lsp-inlayhints.nvim") },
-            },
-            server = opts,
-            dap = {
-              adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
-            },
-          })
-          return true
-        end,
       },
     },
   },
@@ -90,16 +54,6 @@ return {
   {
     "mfussenegger/nvim-dap",
     optional = true,
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
-        opts = function(_, opts)
-          vim.list_extend(opts.ensure_installed, {
-            "codelldb",
-          })
-        end,
-      },
-    },
     opts = function()
       local dap = require("dap")
       dap.configurations.rust = vim.deepcopy(dap.configurations.c)
@@ -118,33 +72,6 @@ return {
           vim.log.levels.ERROR
         )
       end
-    end,
-  },
-
-  {
-    "nvim-neotest/neotest",
-    optional = true,
-    dependencies = {
-      "rouge8/neotest-rust",
-    },
-    opts = {
-      adapters = {
-        ["neotest-rust"] = {},
-      },
-    },
-  },
-
-  {
-    "Saecki/crates.nvim",
-    event = "BufRead Cargo.toml",
-    opts = {
-      null_ls = { enabled = true },
-    },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "crates" })
     end,
   },
 
