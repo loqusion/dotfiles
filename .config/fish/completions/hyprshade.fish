cb0b492a7aa5da9d2027ab0b60cefa1a863df026
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-07-11 06:34:23 -0500

    feat(shell): hyprshade completions

---
+++ .config/fish/completions/hyprshade.fish
@@ -0,0 +1,1 @@
+complete --command hyprshade --no-files --arguments "(env _HYPRSHADE_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) hyprshade)" --condition "env _HYPRSHADE_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) hyprshade"
