ae884d210413f0f564e8a3c19cf7b0af1102bb1e
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-07-15 07:01:30 -0500

    feat(vim): add `OverseerForm` to blankline ignored

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -390,7 +390,7 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     opts = function(_, opts)
-      vim.list_extend(opts.filetype_exclude, { "man", "toggleterm" })
+      vim.list_extend(opts.filetype_exclude, { "OverseerForm", "man", "toggleterm" })
     end,
   },
   {
