a80488a9e1944dd41d73df91e77c242095a543b7
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-07-21 08:48:15 -0500

    fix(terminal): font adjusting keybinds causing blocking

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -16,4 +16,15 @@ config.scrollback_lines = 10000
 -- temporary workaround for https://github.com/wez/wezterm/issues/1701
 config.enable_wayland = false
 
+-- fix: font size adjustment blocking on wayland tiling compositors
+-- for default value, see https://wezfurlong.org/wezterm/config/lua/config/tiling_desktop_environments.html
+config.tiling_desktop_environments = config.tiling_desktop_environments
+  or {
+    'X11 LG3D',
+    'X11 bspwm',
+    'X11 i3',
+    'X11 dwm',
+  }
+table.insert(config.tiling_desktop_environments, 'X11 wlroots wm')
+
 return compose(config, { color_scheme, font, tab, keymaps, window })
