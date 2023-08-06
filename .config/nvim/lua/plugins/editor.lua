0db772c32a4978db17eb0570ee180ddf415ceeea
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-08-06 18:28:28 -0500

    fix(vim): really unbind `S`

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -107,11 +107,17 @@ return {
 
   {
     "folke/flash.nvim",
-    -- stylua: ignore
-    keys = {
-      { "m", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "S", false },
-    },
+    keys = function()
+      -- stylua: ignore
+      return {
+        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+        -- disable S for visual mode
+        { "S", mode = { "n", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      }
+    end,
   },
 
   -- motions respect camelCase, snake_case, etc (using for inner word textobjects)
