f7868aa28f88529f16ce6e5a462e842524bc4101
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-08-10 23:46:25 -0500

    refactor(vim): move conjure plugin config to extras

---
+++ .config/nvim/lua/plugins/extras/tasks/conjure.lua
@@ -0,0 +1,46 @@
+return {
+  -- conjure
+  {
+    "Olical/conjure",
+    event = "VeryLazy",
+    init = function()
+      require("lazyvim.util").on_load("which-key.nvim", function()
+        require("which-key").register({
+          mode = { "n" },
+          ["<localleader>e"] = { name = "+evaluate" },
+          ["<localleader>ec"] = { name = "+comment" },
+          ["<localleader>l"] = { name = "+log" },
+          -- ["<localleader>c"] = { name = "+repl" },
+          -- ["<localleader>r"] = { name = "+reset" },
+        })
+      end)
+      vim.g["conjure#mapping#prefix"] = "<localleader>"
+      vim.g["conjure#mapping#doc_word"] = false
+      vim.g["conjure#mapping#def_word"] = false
+
+      vim.g["conjure#extract#tree_sitter#enabled"] = true
+
+      -- TODO: add virtualenv support
+      vim.g["conjure#client#python#stdio#command"] = "python3 -iq"
+
+      vim.api.nvim_create_autocmd("BufNewFile", {
+        group = vim.api.nvim_create_augroup("conjure_log_disable_lsp", { clear = true }),
+        pattern = { "conjure-log-*" },
+        callback = function(event)
+          vim.diagnostic.disable(event.buf)
+        end,
+        desc = "Conjure Log disable LSP diagnostics",
+      })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "PaterJason/cmp-conjure" },
+    },
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "conjure" } }))
+    end,
+  },
+}
--- .config/nvim/lua/plugins/tasks.lua
+++ .config/nvim/lua/plugins/tasks.lua
@@ -95,59 +95,6 @@ return {
     end,
   },
 
-  -- conjure
-  {
-    "Olical/conjure",
-    enabled = false,
-    event = "VeryLazy",
-    init = function()
-      require("lazyvim.util").on_load("which-key.nvim", function()
-        require("which-key").register({
-          mode = { "n" },
-          ["<localleader>e"] = { name = "+evaluate" },
-          ["<localleader>ec"] = { name = "+comment" },
-          ["<localleader>l"] = { name = "+log" },
-          -- ["<localleader>c"] = { name = "+repl" },
-          -- ["<localleader>r"] = { name = "+reset" },
-        })
-      end)
-      vim.g["conjure#mapping#prefix"] = "<localleader>"
-      vim.g["conjure#mapping#doc_word"] = false
-      vim.g["conjure#mapping#def_word"] = false
-
-      vim.g["conjure#extract#tree_sitter#enabled"] = true
-
-      -- TODO: add virtualenv support
-      vim.g["conjure#client#python#stdio#command"] = "python3 -iq"
-
-      vim.api.nvim_create_autocmd("BufNewFile", {
-        group = vim.api.nvim_create_augroup("conjure_log_disable_lsp", { clear = true }),
-        pattern = { "conjure-log-*" },
-        callback = function(event)
-          vim.diagnostic.disable(event.buf)
-        end,
-        desc = "Conjure Log disable LSP diagnostics",
-      })
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      {
-        "PaterJason/cmp-conjure",
-        cond = function()
-          return Utils.has("conjure")
-        end,
-      },
-    },
-    opts = function(_, opts)
-      if Utils.has("conjure") then
-        local cmp = require("cmp")
-        opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "conjure" } }))
-      end
-    end,
-  },
-
   -- rest client
   {
     "rest-nvim/rest.nvim",
