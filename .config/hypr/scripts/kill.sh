db2c3a7f114ae9187c51f563976fe6491f080a2f
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-08-13 11:11:45 -0500

    feat(hypr): don't kill megasync

--- .config/hypr/scripts/kill.sh
+++ .config/hypr/scripts/kill.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+should_unmap() {
+	[[ "$(hyprctl activewindow -j | jq -r ".class")" =~ Steam|MEGAsync ]]
+}
+
 # http://wiki.hyprland.org/Configuring/Uncommon-tips--tricks/#minimize-steam-instead-of-killing
-if [ "$(hyprctl activewindow -j | jq -r ".class")" = "Steam" ]; then
+if should_unmap; then
 	xdotool getactivewindow windowunmap
 else
 	hyprctl dispatch killactive ""
