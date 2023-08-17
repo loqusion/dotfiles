f6d3c744ec1b2f04eab3dce09db667e150fad690
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-08-17 06:07:00 -0500

    feat(hypr): add some shaders

---
+++ .config/hypr/shaders/blur.glsl
@@ -0,0 +1,35 @@
+#version 330 core
+
+precision mediump float;
+varying vec2 v_texcoord;
+uniform sampler2D tex;
+
+uniform int blurRadius = 1;
+
+void main() {
+	if (blurRadius <= 0) {
+		gl_FragColor = texture(tex, v_texcoord);
+		return;
+	}
+
+	vec2 invTextureSize = 1.0f / textureSize(tex, 0);
+	float invBlurRadius = 1.0f / float(blurRadius);
+
+	float samples = 0.0f;
+	vec4 colorSum = vec4(0.0f);
+	
+	for (int x = -blurRadius; x <= blurRadius; ++x) {
+		for (int y = -blurRadius; y <= blurRadius; ++y) {
+			vec2 offset = vec2(x, y) * invTextureSize;
+
+			float strength = 1 - (length(offset) * invBlurRadius);
+			samples += strength;
+
+			vec2 coords = v_texcoord + offset;
+			colorSum += texture(tex, coords) * strength;
+		}
+	}
+	
+	colorSum /= samples;
+	gl_FragColor = colorSum;
+}
