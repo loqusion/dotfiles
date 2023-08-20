7d6e94e2426ee827f753101747158479de598dbd
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-08-19 22:49:26 -0500

    feat(statusbar): eww launch.sh

--- .config/eww/launch.sh
+++ .config/eww/launch.sh
@@ -2,31 +2,31 @@
 
 THEME=${1:-"default"}
 CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/eww/$THEME"
+EWW_CMD="eww --config $CONFIG_DIR"
+
+primary_monitor_id() {
+	local primary_monitor_name
+	primary_monitor_name=$(xrandr --listmonitors | grep '\*' | awk -F' ' '{print $NF}')
+	hyprctl monitors -j |
+		jq -r \
+			--arg PRIMARY_MONITOR_NAME "$primary_monitor_name" \
+			'.[] | select(.name == $PRIMARY_MONITOR_NAME) | .id'
+}
 
 killall -q eww
 while pgrep -x eww >/dev/null; do sleep 1; done
 
-if ! pgrep -x eww; then
-	eww --config "$CONFIG_DIR" daemon
-	sleep 1
+${EWW_CMD} daemon && sleep 1
+
+PRIMARY_MONITOR_ID=$(primary_monitor_id)
+if [ -n "$PRIMARY_MONITOR_ID" ]; then
+	${EWW_CMD} open "sidebar$PRIMARY_MONITOR_ID"
+else
+	notify-send "eww" "No primary monitor found"
 fi
 
-eww --config "$CONFIG_DIR" open "sidebar0"
-
-## Open standard widgets and bar
-mapfile -t NB_MONITORS < <(hyprctl monitors -j | jq -r '.[] | .id')
-for id in "${NB_MONITORS[@]}"; do
-	bar="bar$id"
-	hyprctl dispatch focusmonitor "$id"
-	eww --config "$CONFIG_DIR" open "$bar"
+for id in $(hyprctl monitors -j | jq -r '.[] | .id'); do
+	${EWW_CMD} open --screen "$id" "bar$id"
 done
 
-# focusmon=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .id')
-# hyprctl dispatch focusmonitor "$focusmon"
-
-# primary display id
-# source "$HOME"/.config/hypr/scripts/variables/load_envs
-
-# Open widgets for primary display
 # use this for notifications : ${EWW} open-many "sidebar$PRIMARY_MONITOR_ID" "notifications$PRIMARY_MONITOR_ID"
-# ${EWW} open "sidebar$PRIMARY_MONITOR_ID"
