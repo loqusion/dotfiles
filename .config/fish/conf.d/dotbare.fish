9665f929ba52a819f3471303df6c5384416d6a06
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-08-22 12:35:21 -0500

    feat(shell): config abbrs

--- .config/fish/conf.d/dotbare.fish
+++ .config/fish/conf.d/dotbare.fish
@@ -1,2 +1,11 @@
 set -Ux DOTBARE_DIR "$HOME/.local/share/dotfiles"
 set -Ux DOTBARE_TREE $HOME
+
+abbr ca "dotbare add -v"
+abbr crs "dotbare reset"
+abbr crm "dotbare rm"
+abbr cmv "dotbare mv"
+abbr crt "dotbare restore"
+abbr ccm "dotbare commit -v"
+abbr cpu "dotbare push -v"
+abbr cpl "dotbare pull -v"
