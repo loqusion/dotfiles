4c3788a60bb1fdff0e80d3334201cc6a23d4799e
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-08-30 04:33:34 -0500

    feat(vim): user commands

---
+++ .config/nvim/lua/config/commands.lua
@@ -0,0 +1,12 @@
+local Utils = require("utils")
+
+local function cmd(name, command, opts)
+  opts = vim.F.if_nil(opts, {})
+  local opts_ = vim.tbl_extend("force", {}, opts or {})
+  vim.api.nvim_create_user_command(name, command, opts_)
+end
+
+cmd("AutoFormatStop", function(t)
+  local root_dir = t.fargs[1] or nil
+  Utils.disable_autoformat(root_dir)
+end, { nargs = "?" })
