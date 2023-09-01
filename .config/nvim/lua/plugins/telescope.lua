50bdd4a569c43e265812e557440088bb4f69add2
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-09-01 18:48:42 -0500

    fix(vim): temporarily use fix for #2667

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,9 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
+    -- TODO: remove this when fix for #2667 is merged
+    -- https://github.com/nvim-telescope/telescope.nvim/pull/2679
+    dev = true,
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
       build = "make",
