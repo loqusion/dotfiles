a9ff38cfd4784a70a286bd1843ac367dd49e0238
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-09-01 18:48:18 -0500

    feat(shell): use `cs` for `dotbare status`

--- .config/fish/conf.d/dotbare.fish
+++ .config/fish/conf.d/dotbare.fish
@@ -1,6 +1,7 @@
 set -Ux DOTBARE_DIR "$HOME/.local/share/dotfiles"
 set -Ux DOTBARE_TREE $HOME
 
+abbr cs "dotbare status"
 abbr ca "dotbare add -v"
 abbr crs "dotbare reset"
 abbr crm "dotbare rm"
@@ -9,4 +10,3 @@ abbr crt "dotbare restore"
 abbr ccm "dotbare commit -v"
 abbr cpu "dotbare push -v"
 abbr cpl "dotbare pull -v --autostash --rebase"
-abbr cst "dotbare status"
