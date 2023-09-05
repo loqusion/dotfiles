27f7a46d1d1ca16e045b77ef809a9a28748f261d
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-09-04 23:42:35 -0500

    fix(shell): `fish_user_paths` overridden

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,6 @@ set -gx SUDO_EDITOR $EDITOR
 set -gx GOPATH $HOME/.go
 set -gx CARGO_HOME $HOME/.cargo
 
-set -Ux fish_user_paths
 fish_add_path $CARGO_HOME/bin
 fish_add_path $GOPATH/bin
 fish_add_path ~/.bin
