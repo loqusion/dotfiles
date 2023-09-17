ab826515983507f7ff26685d9041b303a7d623ef
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-09-17 17:51:48 -0500

    feat(vim): godot

---
+++ .config/nvim/lua/plugins/extras/lang/godot.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "gdtoolkit")
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gdscript = {},
+      },
+      -- setup = {
+      --   gdscript = function(_, opts)
+      --     opts.capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+      --   end,
+      -- },
+    },
+  },
+}
