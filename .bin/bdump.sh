42f92c4a6003977e7b7d70e69ae264670d9d098d
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-10-12 13:09:17 -0500

    fix(topgrade): save and restore ref for bootstrap script

--- .bin/bdump.sh
+++ .bin/bdump.sh
@@ -2,16 +2,24 @@
 set -e
 
 BOOTSTRAP_DIR=~/.local/share/bootstrap
+MAIN_BRANCH=main
 basename=$(basename "$0")
 
 _git() {
 	git -C "$BOOTSTRAP_DIR" "$@"
 }
 
-_git stash push --quiet --include-untracked --no-keep-index --message "stash created by $basename"
-trap '_git stash pop --index &>/dev/null || true' EXIT
+saved_ref=$(_git rev-parse HEAD)
+finish() {
+	set +e
+	_git checkout -q "$saved_ref"
+	_git stash pop --index
+}
+trap finish EXIT
+_git stash push -q --include-untracked --no-keep-index --message "stash created by $basename"
+_git checkout -q "$MAIN_BRANCH"
 
 _git pull --rebase
 "$BOOTSTRAP_DIR"/dump.sh
-_git commit -q -m "chore($(hostname -s)): dump" || true
+_git commit -q --message "chore($(hostname -s)): dump" || true
 _git push
