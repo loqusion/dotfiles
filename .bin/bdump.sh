84d579d955955ffd1978195a1ccd32cedbbbf1a4
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-10-12 12:50:27 -0500

    fix(topgrade): git pull

--- .bin/bdump.sh
+++ .bin/bdump.sh
@@ -11,6 +11,7 @@ _git() {
 _git stash push --quiet --include-untracked --no-keep-index --message "stash created by $basename"
 trap '_git stash pop --index &>/dev/null || true' EXIT
 
+_git pull --rebase
 "$BOOTSTRAP_DIR"/dump.sh
 _git commit -q -m "chore($(hostname -s)): dump" || true
 _git push
