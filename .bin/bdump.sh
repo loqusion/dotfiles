572826e0d874e59bd4f14fcf22fa28cf63ffeb1a
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-10-12 12:43:56 -0500

    feat(topgrade): dump files

---
+++ .bin/bdump.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+BOOTSTRAP_DIR=~/.local/share/bootstrap
+basename=$(basename "$0")
+
+_git() {
+	git -C "$BOOTSTRAP_DIR" "$@"
+}
+
+_git stash push --quiet --include-untracked --no-keep-index --message "stash created by $basename"
+trap '_git stash pop --index &>/dev/null || true' EXIT
+
+"$BOOTSTRAP_DIR"/dump.sh
+_git commit -q -m "chore($(hostname -s)): dump" || true
+_git push
