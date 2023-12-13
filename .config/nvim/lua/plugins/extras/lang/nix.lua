14f840d8d3e89256488ed0ee765ec7639c3f54cd
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-12-13 14:09:44 -0600

    feat(vim): basic nix stuff

---
+++ .config/nvim/lua/plugins/extras/lang/nix.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "nil",
+      })
+    end,
+  },
+
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        nix = { "nixfmt" },
+      },
+    },
+  },
+}
