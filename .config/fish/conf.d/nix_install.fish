ebfe0b70158322e964db2dfead93ba7635fc71f6
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-12-29 14:51:49 -0600

    feat(shell): improve home-manager installation

--- .config/fish/conf.d/nix_install.fish
+++ .config/fish/conf.d/nix_install.fish
@@ -9,27 +9,17 @@ end
 
 function install-home-manager
     if command -v home-manager &>/dev/null
-        # return
+        echo "home-manager is already installed."
+        return
     end
 
     if not install-nixpkgs
         return
     end
 
-    echo "Installing home-manager..."
-    if not nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager; and nix-channel --update
-        return
-    end
-
     install-home-manager-dotfiles
 
-    nix-shell '<home-manager>' -A install
-    set -l final_status $status
-    echo
-
-    echo
-    echo "Done installing home-manager. Please run 'home-manager switch' to activate it."
-    echo
-
-    return $final_status
+    if not nix-shell -p home-manager --run "home-manager switch"
+        return
+    end
 end
