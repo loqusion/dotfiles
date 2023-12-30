5bd66ac27c1133b4bc19852eeb51fb02a4764759
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-12-29 18:33:12 -0600

    feat(shell): extirpate all annoying things about installing nix and home-manager

--- .config/fish/conf.d/nix_install.fish
+++ .config/fish/conf.d/nix_install.fish
@@ -19,11 +19,34 @@ function install-home-manager
 
     install-home-manager-dotfiles
 
-    if not nix-shell -p home-manager --run "home-manager switch"
-        return
+    # don't show annoying notification
+    nix-shell -p home-manager --run "home-manager news >/dev/null 2>&1"
+
+    nix-shell -p home-manager --run "home-manager switch"
+    set -l final_status $status
+
+
+    # I'm so tired
+
+    function delete_if_exist -a paths
+        if test (count $paths) -gt 0
+            rm -rf $paths
+        end
     end
 
-    # remove useless shit
-    rm -rf ~/.local/state/nix/profile/bin/man*
-    rm -rf ~/.local/state/nix/profile/bin/apropos
+    set -l xdg_state_home $XDG_STATE_HOME
+    test -z $xdg_state_home; and set xdg_state_home ~/.local/state
+    set -l nix_profile_xdg $xdg_state_home/nix/profile
+    set -l nix_profile ~/nix-profile
+
+    delete_if_exist (path filter $nix_profile_xdg/bin/man*)
+    delete_if_exist (path filter $nix_profile_xdg/bin/apropos)
+    delete_if_exist (path filter $nix_profile/bin/man*)
+    delete_if_exist (path filter $nix_profile/bin/apropos)
+
+    return $final_status
+end
+
+function uninstall-home-manager
+    yes | nix-shell -p home-manager --run "home-manager uninstall 2>/dev/null"
 end
