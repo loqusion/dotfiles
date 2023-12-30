4f68fb736c13b77725180058de94fb8de4502aef
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-12-29 18:53:55 -0600

    feat(shell): don't have to `man`ually delete man binaries any more

--- .config/fish/conf.d/nix_install.fish
+++ .config/fish/conf.d/nix_install.fish
@@ -23,28 +23,6 @@ function install-home-manager
     nix-shell -p home-manager --run "home-manager news >/dev/null 2>&1"
 
     nix-shell -p home-manager --run "home-manager switch"
-    set -l final_status $status
-
-
-    # I'm so tired
-
-    function delete_if_exist -a paths
-        if test (count $paths) -gt 0
-            rm -rf $paths
-        end
-    end
-
-    set -l xdg_state_home $XDG_STATE_HOME
-    test -z $xdg_state_home; and set xdg_state_home ~/.local/state
-    set -l nix_profile_xdg $xdg_state_home/nix/profile
-    set -l nix_profile ~/nix-profile
-
-    delete_if_exist (path filter $nix_profile_xdg/bin/man*)
-    delete_if_exist (path filter $nix_profile_xdg/bin/apropos)
-    delete_if_exist (path filter $nix_profile/bin/man*)
-    delete_if_exist (path filter $nix_profile/bin/apropos)
-
-    return $final_status
 end
 
 function uninstall-home-manager
