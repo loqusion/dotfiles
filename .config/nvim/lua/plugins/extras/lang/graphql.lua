a5e02a2a667f207163641a4241fb7ef597761a6a
loqusion <38332081+loqusion@users.noreply.github.com>
2024-01-12 03:50:31 -0600

    feat(vim): graphql language server

---
+++ .config/nvim/lua/plugins/extras/lang/graphql.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "graphql-language-service-cli",
+      })
+    end,
+  },
+}
