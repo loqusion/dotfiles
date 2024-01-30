ebddc9ad6bd5b3c75b976a167112854ab4b61498
loqusion <38332081+loqusion@users.noreply.github.com>
2024-01-30 14:23:15 -0600

    feat(scripts): withnotify.sh propagate error code

--- .config/hypr/scripts/withnotify.sh
+++ .config/hypr/scripts/withnotify.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+if [ $# -eq 0 ]; then
+	echo "Usage: $0 <command>" >&2
+	exit 1
+fi
+
 APP=${APP:-$(basename "${1:?not defined}")}
 
-if ! output=$("$@" 2>&1); then
+output=$("$@" 2>&1)
+status=$?
+if [ $status -ne 0 ]; then
 	notify-send --expire-time=5000 --app-name="$APP" --transient "$APP failed" "$output"
+	exit $status
 fi
