1892a11712c2841d4aa85245f65b52b7293330c7
loqusion <38332081+loqusion@users.noreply.github.com>
2024-01-30 16:47:47 -0600

    feat(scripts): improve cast.sh

--- .config/hypr/scripts/cast.sh
+++ .config/hypr/scripts/cast.sh
@@ -1,11 +1,48 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
+SCREENCAST_DIR=${SCREENCAST_DIR:-${XDG_VIDEOS_DIR:-$(xdg-user-dir VIDEOS)}/Screencast}
+
+DATE_COMMAND=${DATE_COMMAND:-"date -Ins"}
+CODEC="gif"
+EXTENSION="gif"
+FPS=30
 
 notify() {
-	notify-send -t 3000 -a 'wf-recorder' -e 'wf-recorder' "$@"
+	notify-send --expire-time=3000 --app-name="cast.sh" --transient "Screencast" "$@"
+}
+
+notify_success() {
+	local file=$1
+	notify --icon="$file" "Saved to ${file}"
+}
+
+get_region() {
+	local windows workspaces rectangles
+	workspaces="$(hyprctl monitors -j | jq -r 'map(.activeWorkspace.id)')"
+	windows="$(hyprctl clients -j | jq -r --argjson workspaces "$workspaces" 'map(select([.workspace.id] | inside($workspaces)))')"
+	rectangles=$(echo "$windows" | jq -r '.[] | "\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')
+
+	echo "$rectangles" | slurp
+}
+
+with_timeout() {
+	timeout --preserve-status 600 "$@"
+}
+
+main() {
+	file="${SCREENCAST_DIR}/$($DATE_COMMAND).${EXTENSION}"
+	mkdir -p "$(dirname "$file")"
+	if ! region=$(get_region); then
+		return 0
+	fi
+
+	with_timeout wf-recorder --geometry "$region" --codec "$CODEC" --filter fps="$FPS" -f "$file"
+
+	notify_success "$file"
 }
 
 if pkill --euid "$USER" --signal SIGINT wf-recorder; then
-	notify 'Saved recording'
 	exit
 fi
 
@@ -13,17 +50,4 @@ fi
 runHook preCast
 trap "runHook postCast" EXIT
 
-XDG_VIDEOS_DIR=${XDG_VIDEOS_DIR:-$(xdg-user-dir VIDEOS)}
-
-# https://github.com/hyprwm/contrib/blob/3126196e7ed609e7c427a39dc126ea067de62a65/grimblast/grimblast#L192-L194
-workspaces="$(hyprctl monitors -j | jq -r 'map(.activeWorkspace.id)')"
-windows="$(hyprctl clients -j | jq -r --argjson workspaces "$workspaces" 'map(select([.workspace.id] | inside($workspaces)))')"
-rectangles=$(echo "$windows" | jq -r '.[] | "\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')
-region=$(echo "$rectangles" | slurp) || {
-	notify 'Cancelled'
-	exit 1
-}
-
-date=$(date +%Y-%m-%d_%H-%M-%S)
-
-timeout 600 wf-recorder -g "$region" -c gif -F fps=30 -f "$XDG_VIDEOS_DIR/$date.gif" || exit 1
+main
