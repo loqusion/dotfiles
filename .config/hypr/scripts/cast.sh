0c2642032efae239e5fbaf1fdf55045aa56431f9
loqusion <38332081+loqusion@users.noreply.github.com>
2024-01-30 16:21:35 -0600

    refactor(scripts): rename vars

--- .config/hypr/scripts/cast.sh
+++ .config/hypr/scripts/cast.sh
@@ -16,13 +16,14 @@ trap "runHook postCast" EXIT
 XDG_VIDEOS_DIR=${XDG_VIDEOS_DIR:-$(xdg-user-dir VIDEOS)}
 
 # https://github.com/hyprwm/contrib/blob/3126196e7ed609e7c427a39dc126ea067de62a65/grimblast/grimblast#L192-L194
-WORKSPACES="$(hyprctl monitors -j | jq -r 'map(.activeWorkspace.id)')"
-WINDOWS="$(hyprctl clients -j | jq -r --argjson workspaces "$WORKSPACES" 'map(select([.workspace.id] | inside($workspaces)))')"
-geometry=$(echo "$WINDOWS" | jq -r '.[] | "\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"' | slurp) || {
+workspaces="$(hyprctl monitors -j | jq -r 'map(.activeWorkspace.id)')"
+windows="$(hyprctl clients -j | jq -r --argjson workspaces "$workspaces" 'map(select([.workspace.id] | inside($workspaces)))')"
+rectangles=$(echo "$windows" | jq -r '.[] | "\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')
+region=$(echo "$rectangles" | slurp) || {
 	notify 'Cancelled'
 	exit 1
 }
 
 date=$(date +%Y-%m-%d_%H-%M-%S)
 
-timeout 600 wf-recorder -g "$geometry" -c gif -F fps=30 -f "$XDG_VIDEOS_DIR/$date.gif" || exit 1
+timeout 600 wf-recorder -g "$region" -c gif -F fps=30 -f "$XDG_VIDEOS_DIR/$date.gif" || exit 1
