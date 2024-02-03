7ec05bab5cf1ea1415dbc1aa1322bd8d922a4a23
loqusion <38332081+loqusion@users.noreply.github.com>
2024-02-02 20:06:25 -0600

    feat(vim): fix mdx formatting

--- .config/nvim/lua/plugins/extras/lang/markdown.lua
+++ .config/nvim/lua/plugins/extras/lang/markdown.lua
@@ -21,22 +21,36 @@ return {
   {
     "stevearc/conform.nvim",
     optional = true,
-    opts = {
-      formatters_by_ft = {
-        markdown = { "injected", "prettierd" },
-        -- ["markdown.mdx"] = { "prettierd" },
-      },
-      formatters = {
-        injected = {
-          options = {
-            lang_to_formatters = {
-              html = {},
-              javascript = {},
-            },
-          },
+    opts = function(_, opts)
+      local injected = require("conform").get_formatter_config("injected")
+
+      return vim.tbl_deep_extend("force", opts, {
+        formatters_by_ft = {
+          ["markdown"] = function(bufnr)
+            local filetypes = vim.split(vim.bo[bufnr].filetype, ".", { plain = true })
+            if vim.list_contains(filetypes, "mdx") then
+              return {
+                "injected_custom",
+                "prettierd",
+              }
+            end
+            return { "injected", "prettierd" }
+          end,
         },
-      },
-    },
+        formatters = {
+          -- formatting MDX with injected will change the semantics unless we override it
+          -- (prettier handles html and javascript formatting anyway)
+          injected_custom = vim.tbl_deep_extend("force", injected, {
+            options = {
+              lang_to_formatters = {
+                html = {},
+                javascript = {},
+              },
+            },
+          }),
+        },
+      })
+    end,
   },
 
   {
