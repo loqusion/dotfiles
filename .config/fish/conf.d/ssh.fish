ade50601435855abfb03aacb4a58f47de24ebc66
loqusion <38332081+loqusion@users.noreply.github.com>
2024-04-16 13:35:57 -0500

    feat(shell): auto idle inhibit on ssh

---
+++ .config/fish/conf.d/ssh.fish
@@ -0,0 +1,9 @@
+function __ssh_idle_inhibit_on_login
+    systemctl --user restart --no-block idleinhibit@$fish_pid.service
+end
+
+function __ssh_idle_inhibit_on_exit --on-event fish_exit
+    systemctl --user stop idleinhibit@fish_pid.service
+end
+
+__ssh_idle_inhibit_on_login
