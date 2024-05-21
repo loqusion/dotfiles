fd77e264ef4b347c6c341e3166c10c8a4864c963
loqusion <38332081+loqusion@users.noreply.github.com>
2024-05-21 11:18:44 -0500

    fix(vim): migrate deprecated function call

--- .config/nvim/lua/utils/debug.lua
+++ .config/nvim/lua/utils/debug.lua
@@ -42,7 +42,7 @@ end
 
 function M.get_value(...)
   local value = { ... }
-  return vim.tbl_islist(value) and vim.tbl_count(value) <= 1 and value[1] or value
+  return vim.islist(value) and vim.tbl_count(value) <= 1 and value[1] or value
 end
 
 function M.switch(config)
