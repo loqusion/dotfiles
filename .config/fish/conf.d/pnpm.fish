ebae3ec274c79ae4d7281c252813c74f85b54dbb
loqusion <38332081+loqusion@users.noreply.github.com>
2024-05-28 15:51:40 -0500

    chore(shell): remove unnecessary lines

--- .config/fish/conf.d/pnpm.fish
+++ .config/fish/conf.d/pnpm.fish
@@ -1,5 +1,2 @@
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-    fish_add_path $PNPM_HOME
-    # set -gx PATH "$PNPM_HOME" $PATH
-end
+fish_add_path $PNPM_HOME
