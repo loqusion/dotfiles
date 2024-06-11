328f4ef1b3bc6e7b11dc4c989edef1b293c2d02e
loqusion <38332081+loqusion@users.noreply.github.com>
2024-06-11 16:49:16 -0500

    feat(vim): enable extras

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -299,27 +299,6 @@ return {
     end,
   },
 
-  -- better increase/decrease
-  {
-    "monaqa/dial.nvim",
-    -- stylua: ignore
-    keys = {
-      { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
-      { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
-    },
-    config = function()
-      local augend = require("dial.augend")
-      require("dial.config").augends:register_group({
-        default = {
-          augend.integer.alias.decimal,
-          augend.integer.alias.hex,
-          augend.date.alias["%m/%d/%Y"],
-          augend.semver.alias.semver,
-        },
-      })
-    end,
-  },
-
   -- display test coverage
   {
     "andythigpen/nvim-coverage",
