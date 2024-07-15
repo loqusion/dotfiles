d32db011bd61a85005f1478b64b9510a448a850c
loqusion <38332081+loqusion@users.noreply.github.com>
2024-07-15 15:25:57 -0500

    feat(vim): add basic dhall support

---
+++ .config/nvim/lua/plugins/extras/lang/dhall.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "dhall-lsp",
+      })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        dhall_lsp_server = {},
+      },
+    },
+  },
+}
