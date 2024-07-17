dbe4ba14b8179e0f8bf268df17e8e51cc91d0c40
loqusion <38332081+loqusion@users.noreply.github.com>
2024-07-16 21:07:12 -0500

    chore(vim): remove lines that were merged into LazyVim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -23,15 +23,6 @@ return {
     ---@class PluginLspOpts
     opts = {
       diagnostics = {
-        -- TODO: Remove when https://github.com/LazyVim/LazyVim/pull/2192 is merged
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = require("lazyvim.config").icons.diagnostics.Error,
-            [vim.diagnostic.severity.WARN] = require("lazyvim.config").icons.diagnostics.Warn,
-            [vim.diagnostic.severity.HINT] = require("lazyvim.config").icons.diagnostics.Hint,
-            [vim.diagnostic.severity.INFO] = require("lazyvim.config").icons.diagnostics.Info,
-          },
-        },
         virtual_text = {
           prefix = "icons",
         },
