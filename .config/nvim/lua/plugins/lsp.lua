491dd26404d95dcd865d7defcca0ea105447285b
loqusion <38332081+loqusion@users.noreply.github.com>
2024-07-16 21:13:41 -0500

    feat(vim): remove lsp-inlayhints.nvim (redundant)

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -106,27 +106,6 @@ return {
     end,
   },
 
-  -- inlay hints
-  {
-    "lvimuser/lsp-inlayhints.nvim",
-    event = "LspAttach",
-    config = function(_, opts)
-      require("lsp-inlayhints").setup(opts)
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("LspAttach_inlayhints", {}),
-        callback = function(args)
-          if not (args.data and args.data.client_id) then
-            return
-          end
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if client then
-            require("lsp-inlayhints").on_attach(client, args.buf)
-          end
-        end,
-      })
-    end,
-  },
-
   -- preview LSP definition
   {
     "rmagatti/goto-preview",
