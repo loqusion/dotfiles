b22235ca10445123ad588a01d590a2df051180fc
loqusion <38332081+loqusion@users.noreply.github.com>
2024-09-22 16:32:53 -0500

    Revert "feat(vim): remove haskell-language-server from mason"
    This reverts commit 64c462a8d60c4de577713da9ab8ab35faca28bca.

--- .config/nvim/lua/plugins/extras/lang/haskell.lua
+++ .config/nvim/lua/plugins/extras/lang/haskell.lua
@@ -1,19 +1,3 @@
----Return the first index matching the given predicate.
----If no items match the predicate, return nil.
----
----@generic T
----@param tbl T[]
----@param predicate fun(t: T): boolean
----@return integer?
-local function tbl_find(tbl, predicate)
-  for i, v in ipairs(tbl) do
-    if predicate(v) then
-      return i
-    end
-  end
-  return nil
-end
-
 return {
   {
     "williamboman/mason.nvim",
@@ -22,14 +6,6 @@ return {
         "hlint",
         "ormolu",
       })
-
-      ---@cast opts { ensure_installed: string[] }
-      local idx = tbl_find(opts.ensure_installed, function(t)
-        return t == "haskell-language-server"
-      end)
-      if idx ~= nil then
-        table.remove(opts.ensure_installed, idx)
-      end
     end,
   },
 
