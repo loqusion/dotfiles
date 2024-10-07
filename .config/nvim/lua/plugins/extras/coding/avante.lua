89d17593f892066733126c8dc5114045eabae999
loqusion <38332081+loqusion@users.noreply.github.com>
2024-10-07 09:26:17 -0500

    feat(vim): add img-clip.nvim as dependency for avante.nvim

--- .config/nvim/lua/plugins/extras/coding/avante.lua
+++ .config/nvim/lua/plugins/extras/coding/avante.lua
@@ -3,6 +3,23 @@ return {
     "yetone/avante.nvim",
     build = LazyVim.is_win() and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" or "make",
     event = "VeryLazy",
+    dependencies = {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
     init = function()
       -- required for `avante.tokenizers` and templates to work
       require("avante_lib").load()
