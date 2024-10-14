16076e83116204ff4c51e7e1d8e00755c8708a29
loqusion <38332081+loqusion@users.noreply.github.com>
2024-10-14 10:33:56 -0500

    feat(shell): `tfo` abbr

--- .config/fish/conf.d/abbr_terraform.fish
+++ .config/fish/conf.d/abbr_terraform.fish
@@ -2,6 +2,7 @@ abbr tf terraform
 abbr tfa 'terraform apply'
 abbr tfd 'terraform destroy'
 abbr tfi 'terraform init'
+abbr tfo 'terraform output'
 abbr tfp 'terraform plan'
 abbr tfr 'terraform apply -refresh-only'
 abbr tfs 'terraform show | bat -l tf'
