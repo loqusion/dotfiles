d6143288174f69518b254a2dbecc6e2f40f15371
loqusion <38332081+loqusion@users.noreply.github.com>
2024-10-23 20:24:57 -0500

    feat(shell): add `--git` to `ll`

--- .config/fish/functions/ll.fish
+++ .config/fish/functions/ll.fish
@@ -1,6 +1,6 @@
-function ll --wraps='eza --color=always --icons --group-directories-first --all --long' --description 'alias ll eza --color=always --icons --group-directories-first --all --long'
+function ll --wraps='eza --color=always --icons --group-directories-first --all --long --git' --description 'alias ll eza --color=always --icons --group-directories-first --all --long'
     if isatty 1 && command -q eza
-        eza --color=always --icons --group-directories-first --all --long $argv
+        eza --color=always --icons --group-directories-first --all --long --git $argv
     else
         command ls -lA --color=always $argv
     end
