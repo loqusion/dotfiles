3216d06ffc70bfeb4ba8174d742dde5202c3589d
loqusion <38332081+loqusion@users.noreply.github.com>
2024-10-29 18:20:50 -0500

    feat(vim): add nginx-language-server (commented)

---
+++ .config/nvim/lua/plugins/extras/lang/nginx.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      -- TODO: enable when nginx-language-server supports Python >=3.12
+      -- vim.list_extend(opts.ensure_installed, { "nginx-language-server" })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- nginx_language_server = {},
+      },
+    },
+  },
+}
