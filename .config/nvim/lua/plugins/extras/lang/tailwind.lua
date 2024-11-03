b9ea778c09669ce106907a64a4cb783ccbcdf9eb
loqusion <38332081+loqusion@users.noreply.github.com>
2024-11-03 02:11:53 -0600

    feat(vim): add cva tailwind lsp

---
+++ .config/nvim/lua/plugins/extras/lang/tailwind.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tailwindcss = {
+          settings = {
+            tailwindCSS = {
+              experimental = {
+                classRegex = {
+                  { "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+                  { "cx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
