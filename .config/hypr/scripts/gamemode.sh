aaac8d78a9c98869f0dee0db9e361489a46556d7
loqusion <38332081+loqusion@users.noreply.github.com>
2024-11-10 19:47:32 -0600

    refactor(hypr): don't use early return

--- .config/hypr/scripts/gamemode.sh
+++ .config/hypr/scripts/gamemode.sh
@@ -3,15 +3,14 @@
 HYPRGAMEMODE=$(hyprctl -j getoption animations:enabled | jq -r '.int')
 
 if [ "$HYPRGAMEMODE" = 1 ]; then
-	hyprctl --batch "\
-    keyword animations:enabled 0;\
-    keyword decoration:drop_shadow 0;\
-    keyword decoration:blur 0;\
-    keyword general:gaps_in 0;\
-    keyword general:gaps_out 0;\
-    keyword general:border_size 1;\
-    keyword decoration:rounding 0"
-	exit
+    hyprctl --batch "\
+        keyword animations:enabled 0;\
+        keyword decoration:drop_shadow 0;\
+        keyword decoration:blur 0;\
+        keyword general:gaps_in 0;\
+        keyword general:gaps_out 0;\
+        keyword general:border_size 1;\
+        keyword decoration:rounding 0"
+else
+    hyprctl reload
 fi
-
-hyprctl reload
