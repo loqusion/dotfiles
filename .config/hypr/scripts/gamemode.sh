c973e421280d2e7d34813de8c102078242207612
loqusion <38332081+loqusion@users.noreply.github.com>
2024-11-14 15:03:18 -0600

    fix(hypr): migrate to 0.45.0
    See https://github.com/hyprwm/Hyprland/releases/tag/v0.45.0

--- .config/hypr/scripts/gamemode.sh
+++ .config/hypr/scripts/gamemode.sh
@@ -5,8 +5,8 @@ HYPRGAMEMODE=$(hyprctl -j getoption animations:enabled | jq -r '.int')
 if [ "$HYPRGAMEMODE" = 1 ]; then
     hyprctl --batch "\
         keyword animations:enabled 0;\
-        keyword decoration:drop_shadow 0;\
-        keyword decoration:blur 0;\
+        keyword decoration:shadow:enabled 0;\
+        keyword decoration:blur:enabled 0;\
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
