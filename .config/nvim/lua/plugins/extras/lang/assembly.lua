b27fcf8e3b006b2cc270492ad03700626150fde1
loqusion <38332081+loqusion@users.noreply.github.com>
2024-11-17 22:18:43 -0600

    feat(vim): assembly support

---
+++ .config/nvim/lua/plugins/extras/lang/assembly.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "asm-lsp", "asmfmt" })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        asm_lsp = {},
+      },
+    },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        asm = { "asmfmt" },
+      },
+    },
+  },
+}
