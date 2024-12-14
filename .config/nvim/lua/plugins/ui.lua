9648f71931f8f1cfb53f1ea42eb069ae0c39c216
loqusion <38332081+loqusion@users.noreply.github.com>
2024-12-14 15:16:44 -0600

    feat(vim): CONTINUE migration to LazyVim 14: use snacks.nvim indent instead of indent-blankline.nvim/mini.indentscope
    See: https://github.com/LazyVim/LazyVim/commit/94fdc42

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -425,37 +425,6 @@ return {
   },
 
   {
-    "lukas-reineke/indent-blankline.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.exclude.filetypes, { "OverseerForm", "man" })
-      opts.scope.enabled = false
-    end,
-  },
-  {
-    "echasnovski/mini.indentscope",
-    version = false,
-    event = "LazyFile",
-    opts = {
-      -- symbol = "▏",
-      symbol = "│",
-      options = { try_as_border = true },
-    },
-    init = function()
-      LazyVim.on_load("indent-blankline.nvim", function()
-        local indent_blankline_opts = LazyVim.opts("indent-blankline.nvim")
-        if indent_blankline_opts.exclude.filetypes then
-          vim.api.nvim_create_autocmd("FileType", {
-            pattern = indent_blankline_opts.exclude.filetypes,
-            callback = function()
-              vim.b.miniindentscope_disable = true
-            end,
-          })
-        end
-      end)
-    end,
-  },
-
-  {
     "carbon-steel/detour.nvim",
     cmd = "Detour",
     keys = {
