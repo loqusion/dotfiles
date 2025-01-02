68f6a8dc6efb97d64dc2f29a45caf6ca25b62f1a
loqusion <38332081+loqusion@users.noreply.github.com>
2025-01-02 11:28:13 -0600

    feat(emacs): more config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,6 +18,32 @@
 (use-package magit
   :bind ("C-x g" . magit-status))
 
+(use-package which-key
+  ;; :diminish which-key-mode
+  :config (which-key-mode))
+
+(defun config-visit ()
+  "Edit Emacs configuration."
+  (interactive)
+  (find-file user-init-file))
+(keymap-global-set "C-c e" 'config-visit)
+
+(defun config-reload ()
+  "Reload Emacs configuration."
+  (interactive)
+  (load-file user-init-file)
+  (condition-case err
+      (progn
+        (load-file user-init-file)
+        (message "Configuration reloaded successfully"))
+    (error "%s" (error-message-string err))))
+(keymap-global-set "C-c r" 'config-reload)
+
+;; Options
+(setq isearch-wrap-pause "no-ding")
+(setq line-number-mode t)
+(setq column-number-mode t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -25,16 +51,19 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
+ '(column-number-mode t)
+ '(global-display-line-numbers-mode t)
  '(inhibit-startup-screen nil)
  '(menu-bar-mode nil)
- '(package-selected-packages '(magit))
+ '(package-selected-packages '(which-key magit))
  '(pos-tip-background-color "#36473A")
  '(pos-tip-foreground-color "#FFFFC8")
+ '(save-place-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 116 :width normal :foundry "1ASC" :family "xos4 Terminus"))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 113 :width normal :foundry "ADBO" :family "SauceCodePro Nerd Font"))))
  '(fringe ((t (:background "#292b2e")))))
