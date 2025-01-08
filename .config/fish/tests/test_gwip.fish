c7340a41f03bdf74ebbb6bd8f4f2b07e50bedd16
loqusion <38332081+loqusion@users.noreply.github.com>
2025-01-07 18:53:37 -0600

    test(shell): update assertion messages

--- .config/fish/tests/test_gwip.fish
+++ .config/fish/tests/test_gwip.fish
@@ -34,14 +34,14 @@ function test_gwip_works
     assert_cmd; or return
 
     test (git ls-files --others --exclude-standard | count) -eq 0
-    assert_cmd 'should be no untracked files'; or return
+    assert_cmd 'expected no untracked files'; or return
     test "$(git rev-list --max-count=1 --grep='^--wip--' HEAD)" = "$(git rev-parse --verify HEAD)"
-    assert_cmd 'commit message should match and be HEAD'; or return
+    assert_cmd 'expected matching commit message to be HEAD'; or return
     assert_snapshot gwip_difftree \
         "$(git diff-tree --no-commit-id --patch HEAD |
             grep -vE '^index [[:alnum:]]*\.\.[[:alnum:]]*$' |
             grep -vE '^new file mode [[:digit:]]*$')" \
-        'only change is the text wip'; or return
+        'expected commit to only include WIP changes'; or return
 end
 
 function test_gunwip_works
