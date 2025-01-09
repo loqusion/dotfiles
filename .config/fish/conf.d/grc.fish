7dccd451f141914b260c42c811591994f91aa40e
loqusion <loqusion@gmail.com>
2025-01-09 17:10:03 -0600

    feat(shell): add `whois` to `grc` commands

--- .config/fish/conf.d/grc.fish
+++ .config/fish/conf.d/grc.fish
@@ -3,7 +3,7 @@ set -U grc_plugin_execs df dig ifconfig \
     wdiff blkid du dnf id ip iostat journalctl kubectl \
     last lsattr lsblk lspci lsmod lsof getfacl getsebool uptime nmap \
     fdisk findmnt free semanage sar ss stat sysctl systemctl \
-    tcpdump tune2fs vmstat w who sockstat
+    tcpdump tune2fs vmstat w who whois sockstat
 
 for executable in $grc_plugin_execs
     if type -q $executable
