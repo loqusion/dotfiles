cd149ea6f1762b5862a91b9ba598ac3840963a6e
loqusion <loqusion@gmail.com>
2025-01-12 00:01:32 -0600

    fix(vim): force `gd` and similar keymaps to use current window

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -50,4 +50,40 @@ return {
       opts.defaults.mappings.i["<C-b>"] = nil
     end,
   },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      if LazyVim.pick.want() ~= "telescope" then
+        return
+      end
+
+      local Keys = require("lazyvim.plugins.lsp.keymaps").get()
+
+      ---@param key string
+      ---@param keys LazyKeysLspSpec[]
+      ---@return LazyKeysLspSpec | nil
+      local function remove_key(key, keys)
+        for i, v in ipairs(keys) do
+          if v[1] == key then
+            return table.remove(keys, i)
+          end
+        end
+      end
+
+      remove_key("gd", Keys)
+      remove_key("gI", Keys)
+      remove_key("gy", Keys)
+
+      -- LazyVim sets `reuse_win` to `true`. When `reuse_win` is `true`, if you
+      -- open a buffer in multiple windows and goto a definition/etc in the
+      -- second window, it will use the first window.
+      -- stylua: ignore
+      vim.list_extend(Keys, {
+        { "gd", function() require("telescope.builtin").lsp_definitions({ reuse_win = false }) end, desc = "Goto Definition", has = "definition" },
+        { "gI", function() require("telescope.builtin").lsp_implementations({ reuse_win = false }) end, desc = "Goto Implementation" },
+        { "gy", function() require("telescope.builtin").lsp_type_definitions({ reuse_win = false }) end, desc = "Goto T[y]pe Definition" },
+      })
+    end,
+  },
 }
