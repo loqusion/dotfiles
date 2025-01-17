1dd9072aed5841c5316704071563f7e37b5a8a44
loqusion <loqusion@gmail.com>
2025-01-17 05:31:42 -0600

    refactor(shell): use single quotes in dotbare abbrs

--- .config/fish/conf.d/dotbare.fish
+++ .config/fish/conf.d/dotbare.fish
@@ -5,16 +5,16 @@ end
 set -Ux DOTBARE_DIR ~/.local/share/dotfiles
 set -Ux DOTBARE_TREE ~
 
-abbr cs "dotbare status"
-abbr ca "dotbare add -v"
-abbr crs "dotbare reset"
-abbr crm "dotbare rm"
-abbr cmv "dotbare mv"
-abbr crt "dotbare restore"
-abbr crt! "dotbare restore --worktree --staged"
-abbr ccm "dotbare commit -v"
-abbr cpu "dotbare push -v"
-abbr cpl "dotbare pull -v --autostash --rebase"
-abbr csus "dotbare submodule status --recursive"
-abbr csuu "dotbare submodule update --progress -j (nproc) --single-branch --init --remote --recursive --rebase"
-abbr cignore "dotbare update-index --assume-unchanged"
+abbr cs 'dotbare status'
+abbr ca 'dotbare add -v'
+abbr crs 'dotbare reset'
+abbr crm 'dotbare rm'
+abbr cmv 'dotbare mv'
+abbr crt 'dotbare restore'
+abbr crt! 'dotbare restore --worktree --staged'
+abbr ccm 'dotbare commit -v'
+abbr cpu 'dotbare push -v'
+abbr cpl 'dotbare pull -v --autostash --rebase'
+abbr csus 'dotbare submodule status --recursive'
+abbr csuu 'dotbare submodule update --progress -j (nproc) --single-branch --init --remote --recursive --rebase'
+abbr cignore 'dotbare update-index --assume-unchanged'
