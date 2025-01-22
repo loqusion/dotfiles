cbb1f66373fb31551f5f80a8ed6bf91ea5d6010f
loqusion <loqusion@gmail.com>
2025-01-22 13:31:56 -0600

    feat(vim): use `sqruff` instead of `sqlfluff`

--- .config/nvim/lua/plugins/extras/lang/sql.lua
+++ .config/nvim/lua/plugins/extras/lang/sql.lua
@@ -1,7 +1,51 @@
 ---@module 'lazy'
 
+local sql_ft = { "sql", "mysql", "plsql" }
+
 ---@type LazySpec[]
 return {
+  -- Remove sqlfluff from upstream config
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      for i, v in ipairs(opts.ensure_installed) do
+        if v == "sqlfluff" then
+          table.remove(opts.ensure_installed, i)
+          break
+        end
+      end
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = function(_, opts)
+      for _, ft in ipairs(sql_ft) do
+        for i, v in ipairs(opts.linters_by_ft[ft]) do
+          if v == "sqlfluff" then
+            table.remove(opts.linters_by_ft[ft], i)
+            break
+          end
+        end
+      end
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.formatters.sqlfluff = nil
+      for _, ft in ipairs(sql_ft) do
+        for i, v in ipairs(opts.formatters_by_ft[ft]) do
+          if v == "sqlfluff" then
+            table.remove(opts.formatters_by_ft[ft], i)
+            break
+          end
+        end
+      end
+    end,
+  },
+
   {
     "tpope/vim-dadbod",
     dependencies = {
@@ -14,23 +58,24 @@ return {
     },
   },
 
-  -- TODO: When https://github.com/stevearc/conform.nvim/pull/625 is merged, use `sqruff`
   {
     "mfussenegger/nvim-lint",
     optional = true,
     opts = function(_, opts)
-      opts.linters.sqlfluff = {
-        args = { "lint", "--format=json" },
-      }
+      for _, ft in ipairs(sql_ft) do
+        opts.linters_by_ft[ft] = opts.linters_by_ft[ft] or {}
+        table.insert(opts.linters_by_ft[ft], "sqruff")
+      end
     end,
   },
   {
     "stevearc/conform.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.formatters.sqlfluff = {
-        args = { "format", "-" },
-      }
+      for _, ft in ipairs(sql_ft) do
+        opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
+        table.insert(opts.formatters_by_ft[ft], "sqruff")
+      end
     end,
   },
 }
