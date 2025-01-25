240c51bd8cbbbd9dcb0f48d846273e71d82ec8ab
loqusion <loqusion@gmail.com>
2025-01-24 19:18:20 -0600

    feat(shell): abbr `yazi` to `y`

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -70,6 +70,8 @@ abbr parup --set-cursor 'paru -Gpa % | bat -l PKGBUILD'
 alias paru-orphans 'paru -Qdtq'
 alias paru-remove-all-orphans 'paru -Qdtq | paru -Rns -'
 
+abbr yazi y
+
 abbr zj zellij
 abbr zja "zellij attach"
 
