d9ccc3a9960278192bf5c908be42faac335b0081
loqusion <loqusion@gmail.com>
2025-03-04 05:16:46 -0600

    feat(vim): enable `matchit` and `matchparen`

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -104,8 +104,6 @@ require("lazy").setup({
     rtp = {
       disabled_plugins = {
         "gzip",
-        "matchit",
-        "matchparen",
         "netrwPlugin",
         "tarPlugin",
         "tohtml",
