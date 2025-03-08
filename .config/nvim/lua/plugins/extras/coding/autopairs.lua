01495e8f2e10a1f08493448d5099a0f0e917aa44
loqusion <loqusion@gmail.com>
2025-03-08 02:36:34 -0600

    feat(vim): remove autopairs rule for single-quotes in Rust
    Most of the time, when I type a single-quote I mean a lifetime.
    Autopairs tends to get in the way in that situation. If I mean a `char`,
    I can type the final single-quote manually with little difficulty.

--- .config/nvim/lua/plugins/extras/coding/autopairs.lua
+++ .config/nvim/lua/plugins/extras/coding/autopairs.lua
@@ -12,7 +12,42 @@ return {
       check_ts = true,
     },
     config = function(_, opts)
-      require("nvim-autopairs").setup(opts)
+      local npairs = require("nvim-autopairs")
+      npairs.setup(opts)
+
+      -- rules
+
+      local cond = require("nvim-autopairs.conds")
+
+      -- don't expand single-quote in Rust
+      ---@type table | nil
+      local rust_single_quote_rules = vim
+        .iter(npairs.get_rules("'"))
+        :filter(function(rule)
+          return #(rule.filetypes or {}) == 1 and rule.filetypes[1] == "rust"
+        end)
+        :fold({}, function(acc, v)
+          if acc == nil or #acc > 0 then
+            return nil
+          else
+            table.insert(acc, v)
+            return acc
+          end
+        end)
+      if rust_single_quote_rules == nil then
+        vim.notify(
+          "Multiple rules satisfied predicate that should have only matched one unique rule!",
+          vim.log.levels.ERROR,
+          { title = "nvim-autopairs (config)" }
+        )
+      elseif #rust_single_quote_rules == 0 then
+        vim.notify("No rules satisfied predicate!", vim.log.levels.ERROR, { title = "nvim-autopairs (config)" })
+      elseif #rust_single_quote_rules > 1 then
+        vim.notify("This should be impossible!", vim.log.levels.ERROR, { title = "nvim-autopairs (config)" })
+      else
+        rust_single_quote_rules[1]:with_pair(cond.none(), 1)
+      end
+
       -- cmp integration
       local cmp = require("cmp")
       local cmp_autopairs = require("nvim-autopairs.completion.cmp")
