54f239a103acab8cfb81aa6138c840210b42b715
John R Bernard <loqusion@gmail.com>
2026-07-25 06:00:54 -0500

    fix(vim): migrate nvim-surround to v4

--- dot_config/nvim/lua/plugins/extras/coding/surround.lua
+++ dot_config/nvim/lua/plugins/extras/coding/surround.lua
@@ -6,42 +6,24 @@ return {
   {
     "kylechui/nvim-surround",
     version = "*",
-    ---@param plugin LazyPlugin
-    ---@param keys LazyKeys
-    keys = function(plugin, keys)
-      ---@cast plugin any
-      local opts = plugin.opts
-      local mappings = {
-        { opts.keymaps.insert, desc = "Insert surrounding", mode = { "i" } },
-        { opts.keymaps.insert_line, desc = "Insert surrounding w/ new lines", mode = { "i" } },
-        { opts.keymaps.normal, desc = "Add surrounding" },
-        { opts.keymaps.normal_cur, desc = "Add surrounding cur line" },
-        { opts.keymaps.normal_line, desc = "Add surrounding w/ new lines" },
-        { opts.keymaps.normal_cur_line, desc = "Add surrounding cur line w/ new lines" },
-        { opts.keymaps.visual, desc = "Add surrounding", mode = { "x" } },
-        { opts.keymaps.visual_line, desc = "Add surrounding w/ new lines", mode = { "x" } },
-        { opts.keymaps.delete, desc = "Delete surrounding" },
-        { opts.keymaps.change, desc = "Change surrounding" },
-      }
-      mappings = vim.tbl_filter(function(m)
-        return m[1] and #m[1] > 0
-      end, mappings)
-
-      return vim.list_extend(mappings, keys)
+    -- v4 dropped the `keymaps` setup option; keymaps are now bound via
+    -- <Plug> mappings when the plugin loads, gated by these globals.
+    -- See `:h nvim-surround.migrating.v3_to_v4`.
+    init = function()
+      vim.g.nvim_surround_no_insert_mappings = true
     end,
+    keys = {
+      { "ys", desc = "Add surrounding" },
+      { "yss", desc = "Add surrounding cur line" },
+      { "yS", desc = "Add surrounding w/ new lines" },
+      { "ySS", desc = "Add surrounding cur line w/ new lines" },
+      { "S", desc = "Add surrounding", mode = "x" },
+      { "gS", desc = "Add surrounding w/ new lines", mode = "x" },
+      { "ds", desc = "Delete surrounding" },
+      { "cs", desc = "Change surrounding" },
+      { "cS", desc = "Change surrounding w/ new lines" },
+    },
     opts = {
-      keymaps = {
-        insert = false,
-        insert_line = false,
-        normal = "ys",
-        normal_cur = "yss",
-        normal_line = "yS",
-        normal_cur_line = "ySS",
-        visual = "S",
-        visual_line = "gS",
-        delete = "ds",
-        change = "cs",
-      },
       highlight = { duration = math.huge },
     },
   },
