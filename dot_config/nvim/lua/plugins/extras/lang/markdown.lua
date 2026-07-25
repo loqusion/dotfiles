adb908a0b150a878f181a6e0f78d0f9592b39422
John R Bernard <loqusion@gmail.com>
2026-07-25 09:52:01 -0500

    feat(vim): get markdownlint to always use home config file

--- dot_config/nvim/lua/plugins/extras/lang/markdown.lua
+++ dot_config/nvim/lua/plugins/extras/lang/markdown.lua
@@ -17,6 +17,11 @@ return {
     "mfussenegger/nvim-lint",
     optional = true,
     opts = {
+      linters = {
+        ["markdownlint-cli2"] = {
+          args = { "--config", vim.fn.expand("~/.markdownlint.yml"), "-" },
+        },
+      },
       linters_by_ft = {
         markdown = { "markdownlint-cli2" },
       },
