ac173630af5bb1b86b53a0665b9758f669dc7e4d
John R Bernard <loqusion@gmail.com>
2026-07-26 15:03:42 -0500

    feat(vim): don't indent in TeX environments

--- dot_config/nvim/lua/plugins/extras/lang/tex.lua
+++ dot_config/nvim/lua/plugins/extras/lang/tex.lua
@@ -8,6 +8,9 @@ return {
       vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
       vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
       vim.g.vimtex_view_method = "skim"
+      -- Don't add a shiftwidth of indent to the body of every \begin{...}/\end{...}
+      -- environment (vimtex's indent script only skips "document" by default).
+      vim.g.vimtex_indent_ignored_envs = { ".*" }
     end,
   },
 }
